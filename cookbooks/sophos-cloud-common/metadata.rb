ad2e3eb9fadb7b69d84e9f0a17cbcb6932cc3d05
Ryan Murphy <ryan.murphy@sophos.com>
2017-12-27 17:24:52 -0500

    XGE-4279 Updated AMI deployment.

--- cookbooks/sophos-cloud-common/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'sophos-cloud-common'
-maintainer       'Sophos'
-license          'All rights reserved'
-description      'Installs/Configures common packages and whatnot'
-version          '1.0.0'
-
-depends 'docker'
