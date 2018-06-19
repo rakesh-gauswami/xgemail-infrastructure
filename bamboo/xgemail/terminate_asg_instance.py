3a42710dc1894a62bf5a1b4e05b0276da966727e
Ryan Murphy <ryan.murphy@sophos.com>
2018-06-19 15:37:46 -0400

    XGE-6027 Moved remaining fluentd config files to new cookbook. Please verify all only_if and not_if statements.

--- bamboo/xgemail/terminate_asg_instance.py
+++ bamboo/xgemail/terminate_asg_instance.py
@@ -26,6 +26,7 @@ import argparse
 import boto3
 import time
 import os
+import botocore
 
 
 # Argument Parser
@@ -52,36 +53,44 @@ def main():
     # session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key) # Option C for running outside of Bamboo
     # Add the names of the associated CloudFormation Stack(s) to perform termination on.
     cfn_stacks = args.asg
-    cf_client = session.client('cloudformation')
+    cf_resource = session.resource('cloudformation')
     asg_client = session.client('autoscaling')
     # Using the CloudFormation Stack name derive the AutoScaling Group name from the Logical Resource ID.
     for stack_name in cfn_stacks:
-        stack_resource = cf_client.describe_stack_resource(StackName=stack_name, LogicalResourceId=args.resource)
-        asg_name = stack_resource['StackResourceDetail']['PhysicalResourceId']
-        print "AutoScalingGroupName: %s" % asg_name
+        try:
+            stack_resource = cf_resource.StackResource(stack_name=stack_name, logical_id=args.resource)
+            asg_name = stack_resource.physical_resource_id
+            print "AutoScalingGroupName: %s" % asg_name
 
-        # Using the AutoScaling Group get the instance-id and terminate the instance in the AutoScaling Group.
-        # Use the Activity that is passed back to monitor the status of the operation.
-        for asg in asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name], MaxRecords=1)['AutoScalingGroups']:
-            instance_id = asg['Instances'][0]['InstanceId']
-            print "InstanceId: %s" % instance_id
-            terminate_response = asg_client.terminate_instance_in_auto_scaling_group(InstanceId=instance_id, ShouldDecrementDesiredCapacity=False)['Activity']
-            progress = terminate_response['Progress']
-            status_code = terminate_response['StatusCode']
-            print "ActivityId: %s"              % terminate_response['ActivityId']
-            print "StatusCode: %s"              % status_code
-            print "Progress: %s"                % progress
+            # Using the AutoScaling Group get the instance-id and terminate the instance in the AutoScaling Group.
+            # Use the Activity that is passed back to monitor the status of the operation.
+            for asg in asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[asg_name], MaxRecords=1)['AutoScalingGroups']:
+                instance_id = asg['Instances'][0]['InstanceId']
+                print "InstanceId: %s" % instance_id
+                terminate_response = asg_client.terminate_instance_in_auto_scaling_group(InstanceId=instance_id, ShouldDecrementDesiredCapacity=False)['Activity']
+                progress = terminate_response['Progress']
+                status_code = terminate_response['StatusCode']
+                print "ActivityId: %s"              % terminate_response['ActivityId']
+                print "StatusCode: %s"              % status_code
+                print "Progress: %s"                % progress
 
-            # Keep checking the AutoScaling Activity until the progress equals 100 and the Status is Successful.
-            while progress != 100 and status_code != 'Successful':
-                time.sleep(10)
-                activities = asg_client.describe_scaling_activities(ActivityIds=[terminate_response['ActivityId']], MaxRecords=1)['Activities']
-                progress = activities[0]['Progress']
-                status_code = activities[0]['StatusCode']
-                print "Description: %s" % activities[0]['Description']
-                print "Progress: %s"    % progress
-                print "StatusCode: %s"  % status_code
-            print "Instance %s has been terminated." % instance_id
+                # Keep checking the AutoScaling Activity until the progress equals 100 and the Status is Successful.
+                while progress != 100 and status_code != 'Successful':
+                    time.sleep(60)
+                    activities = asg_client.describe_scaling_activities(ActivityIds=[terminate_response['ActivityId']], MaxRecords=1)['Activities']
+                    progress = activities[0]['Progress']
+                    status_code = activities[0]['StatusCode']
+                    print "Description: %s" % activities[0]['Description']
+                    print "Progress: %s"    % progress
+                    print "StatusCode: %s"  % status_code
+                print "Instance %s has been terminated." % instance_id
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == 'Throttling':
+                time.sleep(60)
+                pass
+            else:
+                raise e
+
 
 if __name__ == "__main__":
     main()
--- cookbooks/sophos-cloud-fluentd/recipes/configure.rb
+++ cookbooks/sophos-cloud-fluentd/recipes/configure.rb
@@ -19,7 +19,7 @@ REGION                = node['sophos_cloud']['region']
 SNS_TOPIC             = node['xgemail']['msg_statistics_rejection_sns_topic']
 
 # All instances - Start Order: 10
-template 'fluentd-source-maillog.conf' do
+template 'fluentd-source-maillog' do
   path "#{CONF_DIR}/10-source-maillog.conf"
   source 'fluentd-source-maillog.conf.erb'
   mode '0644'
@@ -27,8 +27,117 @@ template 'fluentd-source-maillog.conf' do
   group 'root'
 end
 
+# internet-submit and customer-submit - Start Order: 10
+template 'fluentd-source-jilter' do
+  path "#{CONF_DIR}/10-source-jilter.conf"
+  source 'fluentd-source-jilter.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+      :application_name => NODE_TYPE
+  )
+  only_if { NODE_TYPE == 'submit' }
+  only_if { NODE_TYPE == 'customer-submit' }
+end
+
+# All instances except extended delivery - Start Order: 10
+template 'fluentd-source-lifecycle' do
+  path "#{CONF_DIR}/10-source-lifecycle.conf"
+  source 'fluentd-source-lifecycle.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+  not_if { NODE_TYPE == 'xdelivery' }
+  not_if { NODE_TYPE == 'internet-xdelivery' }
+end
+
+# internet-delivery - Start Order: 10
+ template 'fluentd-source-messagebouncer' do
+   path "#{CONF_DIR}/10-source-messagebouncer.conf"
+   source 'fluentd-source-messagebouncer.conf.erb'
+   mode '0644'
+   owner 'root'
+   group 'root'
+   variables(
+     :application_name => NODE_TYPE
+   )
+   only_if { NODE_TYPE == 'internet-delivery' }
+ end
+
+# internet-submit - Start Order: 10
+template 'fluentd-source-multi-policy' do
+  path "#{CONF_DIR}/10-source-multi-policy.conf"
+  source 'fluentd-source-multi-policy.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+  only_if { NODE_TYPE == 'submit' }
+end
+
+# internet-submit and customer-submit - Start Order: 10
+template 'fluentd-source-policy' do
+  path "#{CONF_DIR}/10-source-policy.conf"
+  source 'fluentd-source-policy.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+  only_if { NODE_TYPE == 'submit' }
+  only_if { NODE_TYPE == 'customer-submit' }
+end
+
+# All delivery instances - Start Order: 10
+template 'fluentd-source-sqsmsgconsumer' do
+  path "#{CONF_DIR}/10-source-sqsmsgconsumer.conf"
+  source 'fluentd-source-sqsmsgconsumer.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+  only_if { NODE_TYPE == 'delivery' }
+  only_if { NODE_TYPE == 'xdelivery' }
+  only_if { NODE_TYPE == 'internet-delivery' }
+  only_if { NODE_TYPE == 'internet-xdelivery' }
+end
+
+# internet-submit and customer-submit - Start Order: 10
+template '/etc/td-agent.d/10-source-sqsmsgproducer.conf' do
+  source 'fluentd-source-sqsmsgproducer.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+  only_if { NODE_TYPE == 'submit' }
+  only_if { NODE_TYPE == 'customer-submit' }
+end
+
+# All instances - Start Order: 10
+template 'fluentd-source-monit' do
+  path "#{CONF_DIR}/10-source-monit.conf"
+  source 'fluentd-source-monit.conf.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+  variables(
+    :application_name => NODE_TYPE
+  )
+end
+
 # All instances except internet-submit  - Start Order: 20
-template 'fluentd-match-maillog.conf' do
+template 'fluentd-match-maillog' do
   path "#{CONF_DIR}/20-match-maillog.conf"
   source 'fluentd-match-maillog.conf.erb'
   mode '0644'
@@ -42,7 +151,7 @@ template 'fluentd-match-maillog.conf' do
 end
 
 # All instances - Start Order: 50
-template 'fluentd-filter-maillog.conf' do
+template 'fluentd-filter-maillog' do
   path "#{CONF_DIR}/50-filter-maillog.conf"
   source 'fluentd-filter-maillog.conf.erb'
   mode '0644'
@@ -55,7 +164,7 @@ template 'fluentd-filter-maillog.conf' do
 end
 
 # Only internet-submit  - Start Order: 60
-template 'fluentd-match-msg-stats.conf' do
+template 'fluentd-match-msg-stats' do
   path "#{CONF_DIR}/60-match-msg-stats.conf"
   source 'fluentd-match-msg-stats.conf.erb'
   mode '0644'
@@ -69,7 +178,7 @@ template 'fluentd-match-msg-stats.conf' do
 end
 
 # All instances - Start Order: 70
-template 'fluentd-filter-transform.conf' do
+template 'fluentd-filter-transform' do
   path "#{CONF_DIR}/70-filter-transform.conf"
   source 'fluentd-filter-transform.conf.erb'
   mode '0644'
@@ -84,7 +193,7 @@ template 'fluentd-filter-transform.conf' do
 end
 
 # All instances - Start Order: 99
-template 'fluentd-match-s3.conf' do
+template 'fluentd-match-s3' do
   path "#{CONF_DIR}/99-match-s3.conf"
   source 'fluentd-match-s3.conf.erb'
   mode '0644'
@@ -98,7 +207,7 @@ template 'fluentd-match-s3.conf' do
 end
 
 # Only internet-submit - Start Order: 99
-template 'fluentd-match-sns-reject.conf' do
+template 'fluentd-match-sns-reject' do
   path "#{CONF_DIR}/99-match-sns-reject.conf"
   source 'fluentd-match-sns-reject.conf.erb'
   mode '0644'
