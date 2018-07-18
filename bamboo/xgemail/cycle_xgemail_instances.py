926c1910bdca279854058c494124d46a0aad1960
Ryan Murphy <ryan.murphy@sophos.com>
2018-07-18 14:22:24 -0400

    XGE-6222 Made small change regarding build result key. Different job different key.

--- bamboo/xgemail/cycle_xgemail_instances.py
+++ bamboo/xgemail/cycle_xgemail_instances.py
@@ -35,7 +35,7 @@ import time
 import logging
 from botocore.exceptions import ClientError, WaiterError
 
-# logging to syslog setup
+# logging to console setup
 logger = logging.getLogger('cycle-instances')
 logger.setLevel(logging.INFO)
 console_handler = logging.StreamHandler()
@@ -44,6 +44,7 @@ formatter = logging.Formatter(
 )
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
+
 
 # Argument Parser
 def parse_command_line():
@@ -54,10 +55,10 @@ def parse_command_line():
 
 
 class XgemailInstance(object):
-    def __init__(self, instance, ami, build):
+    def __init__(self, instance, ami, plan_key, build_number):
         tags = {tag['Key']: tag['Value'] for tag in instance.tags}
         self.current_ami = ami
-        self.current_build_result_key = build
+        self.current_build_result_key = '-'.join([plan_key, 'JOB1', build_number])
         self.image_id = instance.image_id
         self.instance_id = instance.instance_id
         self.asg = tags.get('aws:autoscaling:groupName')
@@ -68,12 +69,18 @@ class XgemailInstance(object):
         self.elb = None
 
     def build_check(self):
+        """
+        Compares the tags of the EC2 instance to the values from the deployment.
+        """
         if self.current_ami not in self.image_id or self.current_build_result_key not in self.build_result_key:
             return True
         else:
             return False
 
     def terminate_asg_instance(self):
+        """
+        Terminate the EC2 Instance through the AutoScaling Group to allow the lifecycle hook to run.
+        """
         logger.info('===>    Terminate instance')
         try:
             terminate_response = asg_client.terminate_instance_in_auto_scaling_group(InstanceId=self.instance_id, ShouldDecrementDesiredCapacity=False)
@@ -82,6 +89,9 @@ class XgemailInstance(object):
             logger.info("Client Error terminating the ASG Instance. {}".format(ce))
 
     def get_new_instance(self):
+        """
+        After termination this is run to get the new EC2 Instance Id from the AutoScaling Group.
+        """
         logger.info('===>    New Instance IDs')
         try:
             for asg in asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[self.asg], MaxRecords=1)['AutoScalingGroups']:
@@ -93,6 +103,9 @@ class XgemailInstance(object):
 
 
 def get_instances(name):
+    """
+    Find Instances based on their Xgemail Instance type, which is in their name.
+    """
     filters = [
         {
             'Name': 'tag:Name',
@@ -107,11 +120,17 @@ def get_instances(name):
 
 
 def termination_control(l, n):
+    """
+    Enables the termination queue to be worked on in a configurable number of chunks as opposed to all at once.
+    """
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
 
 def wait_for_instance_terminated(instance_list):
+    """
+    EC2 Waiter that waits for a list of Instances to be terminated before moving on.
+    """
     try:
         ec2_client.get_waiter('instance_terminated').wait(
             InstanceIds=[i.instance_id for i in instance_list],
@@ -127,6 +146,9 @@ def wait_for_instance_terminated(instance_list):
 
 
 def wait_for_instance_running(instance_list):
+    """
+    EC2 Waiter that waits for a list of Instances to be in running state before moving on.
+    """
     time.sleep(5)
     try:
         ec2_client.get_waiter('instance_running').wait(
@@ -144,6 +166,9 @@ def wait_for_instance_running(instance_list):
 
 
 def wait_for_instance_status_ok(instance_list):
+    """
+    EC2 Waiter that waits for a list of Instances to be in OK status before moving on.
+    """
     time.sleep(5)
     try:
         ec2_client.get_waiter('instance_status_ok').wait(
@@ -161,6 +186,9 @@ def wait_for_instance_status_ok(instance_list):
 
 
 def wait_for_instance_in_service(instance_list):
+    """
+    ELB Waiter that waits for a list of Instances to be in service before moving on.
+    """
     time.sleep(5)
     try:
         elb_client.get_waiter('instance_in_service').wait(
@@ -185,9 +213,10 @@ if __name__ == "__main__":
     aws_secret_access_key = os.environ['bamboo_custom_aws_secretAccessKey_password']
     aws_session_token = os.environ['bamboo_custom_aws_sessionToken_password']
     region = os.environ['bamboo_vpc_REGION']
-    build_result_key = os.environ['bamboo_buildResultKey']
+    bamboo_plan_key = os.environ['bamboo_planKey']
+    bamboo_build_number = os.environ['bamboo_buildNumber']
     ami_id = os.environ['bamboo_xgemail_ami_id']
-    session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=region)
+    session = boto3.Session(aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token,region_name=region)
 
     ec2 = session.resource('ec2')
     """:type: pyboto3.ec2 """
@@ -210,7 +239,7 @@ if __name__ == "__main__":
         instances = get_instances(name=instance_type)
         xinstances = list()
         for i in instances:
-            xi = XgemailInstance(instance=i, ami=ami_id, build=build_result_key)
+            xi = XgemailInstance(instance=i, ami=ami_id, plan_key=bamboo_plan_key, build_number=bamboo_build_number)
             xinstances.append(xi)
         termination_queue = [x for x in xinstances if x.terminate]
         while len(termination_queue) != 0:
