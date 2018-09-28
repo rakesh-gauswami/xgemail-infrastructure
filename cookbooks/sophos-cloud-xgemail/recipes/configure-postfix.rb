262f9ce03bff0942dc5478e83e093f11e53c6ada
Felix Rohrer <Felix.Rohrer@Sophos.com>
2018-09-28 11:53:39 -0400

    XGE-7014: Added configuration to multi-instance config

--- cookbooks/sophos-cloud-xgemail/recipes/configure-postfix.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-postfix.rb
@@ -21,10 +21,7 @@ CONFIGURATION_COMMANDS =
   node['xgemail']['common_instance_config_params'] +
   [
     # Disable inet services for default instance
-    'master_service_disable = inet',
-
-    # Increase default processing limit from default 100 to 200
-    'default_process_limit = 200'
+    'master_service_disable = inet'
   ]
 
 CONFIGURATION_COMMANDS_SANDBOX =
