6889dc468ecdc73c0061c856ce1341e82ca457b6
Visal Acharya <visal.acharya@sophos.com>
2018-10-16 15:02:11 -0400

    XGE-7271 HAProxy patch for mail-service

---
+++ docker/sophos_cloud_haproxy/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+export haproxy_file="${HOME}/g/nova/haproxy/haproxy.cfg"
+export addendum_file="././config/xgemail_addendum_haproxy.cfg"
+{
+    # modify existing routing to mail-backend
+    edited = $(sed 's/hub-backend if mail_context/mail-backend if mail_context/' ${haproxy_file})
+
+    if [[ ${edited} == 0 ]]; then
+        # add mail-backend
+        cat ${addendum_file} >> ${haproxy_file}
+
+        # restart nova proxy, make sure mail-service is up
+        docker restart nova_proxy_1
+    fi;
+
+} || exit 1
