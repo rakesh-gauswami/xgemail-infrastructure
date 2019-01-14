2b6071e1d5c965a94eb89bbd5c010d5ef46b5ba6
Sergiy Talamanov <sergiy.talamanov@sophos.com>
2019-01-14 16:12:27 +0200

    XGE-8261 Python script update to take care of reply delivery issue.

--- cookbooks/sophos-cloud-xgemail/recipes/setup_customer_delivery_transport_updater_cron.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_customer_delivery_transport_updater_cron.rb
@@ -33,6 +33,8 @@ XGEMAIL_FILES_DIR     = node['xgemail']['xgemail_files_dir']
 TRANSPORT_FILENAME    = 'transport'
 MAIL_PIC_API_RESPONSE_TIMEOUT = node['xgemail']['mail_pic_apis_response_timeout_seconds']
 MAIL_PIC_API_AUTH     = node['xgemail']['mail_pic_api_auth']
+POLICY_BUCKET         = node['xgemail']['xgemail_policy_bucket_name']
+ENC_CONFIG_KEY        = 'config/outbound-relay-control/encryption/'
 
 CONFIGURATION_COMMANDS =
   [
@@ -101,6 +103,8 @@ template CRON_SCRIPT_PATH do
     :mail_pic_api_response_timeout => MAIL_PIC_API_RESPONSE_TIMEOUT,
     :mail_pic_api_auth => MAIL_PIC_API_AUTH,
     :connections_bucket => CONNECTIONS_BUCKET
+    :policy_bucket => POLICY_BUCKET
+    :enc_config_key => ENC_CONFIG_KEY
   )
   notifies :run, "execute[#{CRON_SCRIPT_PATH}]", :immediately
 end
