d369ea47440b62ef9f9ca4cb52019383e0bf9624
Kiran Pinna <Kiran.Pinna@Sophos.com>
2019-01-22 11:07:56 -0500

    XGE-6217 outbound monitor receive file

--- lambda/xgemail_outbound_monitor_receive.js
+++ lambda/xgemail_outbound_monitor_receive.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Sophos Limited. All rights reserved.
+ * Copyright 2019 Sophos Limited. All rights reserved.
  *
  * 'Sophos' and 'Sophos Anti-Virus' are registered trademarks of Sophos Limited and Sophos Group. All other product
  * and company names mentioned are trademarks or registered trademarks of their respective owners.
