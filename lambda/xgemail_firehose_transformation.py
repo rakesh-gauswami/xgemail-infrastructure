1c492421c4cd10c68c3dedec5ba1c45e8b976ad3
Ryan Murphy <ryan.murphy@sophos.com>
2019-04-19 14:08:44 -0400

    XGE-10408 Initial Commit.

---
+++ lambda/xgemail_firehose_transformation.py
@@ -0,0 +1,55 @@
+"""
+AWS Lambda Function that is triggered from Kinesis Firehose
+to transform log messages.
+
+Copyright 2019, Sophos Limited. All rights reserved.
+
+'Sophos' and 'Sophos Anti-Virus' are registered trademarks of
+Sophos Limited and Sophos Group.  All other product and company
+names mentioned are trademarks or registered trademarks of their
+respective owners.
+"""
+
+from __future__ import print_function
+
+import json
+import base64
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def process_records(records):
+    """
+    Process each log from a batch of logs
+    """
+    for record in records:
+        raw_data = base64.b64decode(record['data'])
+        data = json.loads(raw_data)
+        record_id = record['recordId']
+        message = data['message']
+
+        if message.startswith('Unknown feature flag'):
+            yield {
+                'result': 'Dropped',
+                'recordId': record_id
+            }
+        else:
+            yield {
+                'data': base64.b64encode(json.dumps(data)),
+                'result': 'Ok',
+                'recordId': record_id
+            }
+
+
+def firehose_transformation_handler(event, context):
+    """
+    Main Lambda Handler
+    """
+    records = list(process_records(event['records']))
+
+    logger.info('Successfully processed {} records.'.format(len(event['records'])))
+
+    return {'records': records}
