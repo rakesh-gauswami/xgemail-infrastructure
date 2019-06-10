b7cb2d1545bb3a39b2e31fbee57a8fe2d5180215
Harsh Soni <harsh.soni@sophos.com>
2019-06-10 16:10:02 +0530

    Removed setup policy recipe for sandbox environment

--- cookbooks/sophos-cloud-xgemail/recipes/configure-customer-submit-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-customer-submit-queue.rb
@@ -184,5 +184,4 @@ else
 
   include_recipe 'sophos-cloud-xgemail::setup_xgemail_sqs_message_producer'
   include_recipe 'sophos-cloud-xgemail::setup_xgemail_utils_structure'
-  include_recipe 'sophos-cloud-xgemail::setup_xgemail_policy_service'
 end
