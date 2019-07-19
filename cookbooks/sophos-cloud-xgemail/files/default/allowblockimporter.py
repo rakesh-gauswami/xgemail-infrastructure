2058259fcedbb54d57eb2d64b9f37ff4d5491250
felixrohrer <felix.rohrer@sophos.com>
2019-07-19 16:08:26 -0400

    XGE-11915: Apply same change to DEV.

--- cookbooks/sophos-cloud-xgemail/files/default/allowblockimporter.py
+++ cookbooks/sophos-cloud-xgemail/files/default/allowblockimporter.py
@@ -32,7 +32,7 @@ except ImportError:
     from prettytable import PrettyTable
 
 # Constants
-MAX_LINE_LENGTH = 61
+MAX_LINE_LENGTH = 63
 MAX_CUSTOMER_ENTRIES = 30000
 MAX_USER_ENTRIES = 10000
 MAX_FILE_SIZE = 9500
@@ -106,6 +106,7 @@ def write_new_file(file_name, content):
 def write_error_file(all_errors):
     """
     Writes a tabular file listing all the entries that could not be imported.
+    Returns number of errors
     """
     t = PrettyTable(['Entry', 'Error'])
     t.align = 'l'
@@ -208,15 +209,17 @@ def import_csv(main_file, customer_id, import_url, region, env):
     all_files = create_csv_files_with_max_size(main_file)
     files_already_uploaded = 0
 
-    print_colorized('Uploading file {}'.format(main_file), Colors.HEADER)
     for cur_file in all_files:
         all_results.append(upload(import_url, cur_file, customer_id, False, region, env))
         files_already_uploaded += 1
-        sys.stdout.write('{:6.2f}% uploaded\r'.format(float(files_already_uploaded)/float(len(all_files)) * 100))
+        sys.stdout.write('Uploading file {0}: {1:6.2f}% uploaded\r'.format(
+            main_file,
+            float(files_already_uploaded)/float(len(all_files)) * 100)
+        )
         sys.stdout.flush()
     print
+    print
 
-    failures = 0
     successful = 0
     all_errors = []
     failed_files = []
@@ -228,43 +231,35 @@ def import_csv(main_file, customer_id, import_url, region, env):
         if result.has_errors():
             errors = result.get_errors()
             all_errors.extend(errors)
-            failures += len(errors)
-
-    indentation = max(len(str(successful)), len(str(failures)))
-
-    print_errors()
 
     print_colorized(
-        '[{}] entries imported successfully'.format(str(successful).rjust(indentation)),
+        '[{}] entries imported successfully'.format(str(successful)),
         Colors.GREEN if successful > 0 else Colors.YELLOW
     )
     if len(failed_files) > 0:
         write_failed_files(failed_files)
         print_colorized(
-            '[{}] files failed to be uploaded. Failure file written to: {}'.format(
-                len(failed_files),
-                FAILED_FILES_PATH
-            ),
+            'Some sections could not be imported. You can attempt to retry these entries by using the --retry flag',
             Colors.YELLOW
         )
-    if (failures > 0):
-        error_file_path = write_error_file(all_errors)
+    if (len(all_errors) > 0):
+        write_error_file(all_errors)
         print_colorized(
-            '[{}] entries failed to import. Report written to: {}'.format(
-                str(failures).rjust(indentation),
+            'Some errors encountered (report also written to {}):'.format(
                 ERROR_ENTRIES_PATH
             ),
             Colors.YELLOW
         )
+        print_errors()
 
 def determine_invalid_rows(main_file):
     """
     Determines any invalid rows from the main CSV file.
     Returns True if any invalid rows found, False otherwise
     """
-    print_colorized('{0} WARNING {0}'.format('#'*26), Colors.HEADER)
-    print_colorized('Maximum entries for customer allow/block entries:\t{}'.format(MAX_CUSTOMER_ENTRIES), Colors.HEADER)
-    print_colorized('Maximum entries for user allow/block entries:\t\t{}'.format(MAX_USER_ENTRIES), Colors.HEADER)
+    print_colorized('{0} WARNING {0}'.format('#'*27), Colors.HEADER)
+    print_colorized('# Maximum entries for customer allow/block entries:\t{} #'.format(MAX_CUSTOMER_ENTRIES), Colors.HEADER)
+    print_colorized('# Maximum entries for user allow/block entries:\t\t{} #'.format(MAX_USER_ENTRIES), Colors.HEADER)
     print_colorized('{}'.format('#'*MAX_LINE_LENGTH), Colors.HEADER)
     print
 
@@ -279,21 +274,21 @@ def determine_invalid_rows(main_file):
             if tokens[0].lower() == 'enterprise':
                 if entries > MAX_CUSTOMER_ENTRIES:
                     print_colorized(
-                        'Customer allow/block entry {} exeeds maximum number of entries ({}): {}'.format(
+                        'Customer allow/block entry {} exeeds maximum number of entries: {}/{}'.format(
                             line_number,
-                            MAX_CUSTOMER_ENTRIES,
-                            entries
+                            entries,
+                            MAX_CUSTOMER_ENTRIES
                         ),
                         Colors.YELLOW
                     )
                     nr_of_invalid_entries += 1
             elif entries > MAX_USER_ENTRIES:
                 print_colorized(
-                    'User allow/block entry {} ({}) exeeds maximum number of entries ({}): {}'.format(
+                    'User allow/block entry {} ({}) exeeds maximum number of entries: {}/{}'.format(
                         line_number,
                         tokens[0],
-                        MAX_USER_ENTRIES,
-                        entries
+                        entries,
+                        MAX_USER_ENTRIES
                     ),
                     Colors.YELLOW
                 )
@@ -303,6 +298,7 @@ def determine_invalid_rows(main_file):
                 'All {} entries are within the above defined limits'.format(line_number - 1),
                 Colors.GREEN
             )
+            print
             return False
         print_colorized(
             'Found a total of {}/{} entries that exceed the above defined limits'.format(
@@ -311,6 +307,7 @@ def determine_invalid_rows(main_file):
             ),
             Colors.YELLOW
         )
+        print
         return True
 
 def delete_all(import_url, customer_id, region, env):
