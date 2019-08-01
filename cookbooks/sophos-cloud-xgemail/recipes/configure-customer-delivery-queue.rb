47dc0540f3ec27257a2f047d23857d3c11503f0b
braddugdale <brad.dugdale@sophos.com>
2019-08-01 12:47:19 -0400

    Fixinf recipe reference

--- cookbooks/sophos-cloud-xgemail/recipes/configure-customer-delivery-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-customer-delivery-queue.rb
@@ -100,5 +100,5 @@ include_recipe 'sophos-cloud-xgemail::setup_customer_delivery_transport_updater_
 include_recipe 'sophos-cloud-xgemail::setup_transport_route_config'
 include_recipe 'sophos-cloud-xgemail::setup_xgemail_sqs_message_consumer'
 
-include_recipe 'sophos-cloud-xgemail::setup_push_policy_toggle'
+include_recipe 'sophos-cloud-xgemail::setup_push_policy_delivery_toggle'
 
