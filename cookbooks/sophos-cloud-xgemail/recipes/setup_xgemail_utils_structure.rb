b837bdd4e7fe1d13a1408c31964414b3c3c962ee
Harsh Soni <harsh.soni@sophos.com>
2019-08-05 17:30:30 +0530

    Updated file to include transportrouteconfig to the instances

--- cookbooks/sophos-cloud-xgemail/recipes/setup_xgemail_utils_structure.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_xgemail_utils_structure.rb
@@ -84,7 +84,8 @@ elsif NODE_TYPE == 'customer-submit'
     owner 'root'
     group 'root'
   end
-elsif NODE_TYPE == 'customer-delivery'
+elsif NODE_TYPE == 'customer-delivery' or NODE_TYPE == 'internet-delivery' or
+       NODE_TYPE == 'risky-delivery' or NODE_TYPE == 'encryption-delivery'
   cookbook_file "#{XGEMAIL_UTILS_DIR}/#{TRANSPORT_ROUTE_CONFIG}" do
     source 'transportrouteconfig.py'
     mode '0644'
