b59112da27707a22b9dac2865ea27cba258aded2
v.prajapati <v.prajapati@sophos.com>
2019-08-12 17:45:52 +0530

    Added X-Sophos-Email-ID header in metadata file.

--- cookbooks/sophos-cloud-xgemail/files/default/test_metadata.py
+++ cookbooks/sophos-cloud-xgemail/files/default/test_metadata.py
@@ -30,7 +30,8 @@ class MetadataTest(unittest.TestCase):
             '42c6b66mZYz1V',
             '2018-10-19T13:37:00Z',
             'r-domain.com',
-            ['a@r-domain.com', 'b@r-domain.com', 'c@r-domain.com']
+            ['a@r-domain.com', 'b@r-domain.com', 'c@r-domain.com'],
+            'e6592dabae0e466c86b1c891eda752d8'
         )
 
     def test_add_uuid_to_queue_id(self):
@@ -57,6 +58,7 @@ class MetadataTest(unittest.TestCase):
             "recipient_domain": "r-domain.com", 
             "queue_id": "42c6b66mZYz1V", 
             "sender_ip": "1.2.3.4",
+            "x_sophos_email_id":"e6592dabae0e466c86b1c891eda752d8",
             "is_microservice_request": False
         }
         self.assertEquals(
