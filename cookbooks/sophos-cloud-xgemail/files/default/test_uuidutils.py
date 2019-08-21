30c986f2b14e4731a6b7bbd36f676a4b41bf3fb4
v.prajapati <v.prajapati@sophos.com>
2019-08-21 22:06:42 +0530

    renamed file to uuidutils, as suggested. updated setup file so that while setup it moves the file to utils folder under xgemail.

--- cookbooks/sophos-cloud-xgemail/files/default/test_uuidutils.py
+++ cookbooks/sophos-cloud-xgemail/files/default/test_uuidutils.py
@@ -15,25 +15,25 @@ respective owners.
 import json
 import re
 import unittest
-import xgemail_helper
+import uuidutils
 
 class XgemailHelperTest(unittest.TestCase):
 
     def test_x_sophos_email_id_empty(self):
-        self.assertEqual(xgemail_helper.get_x_sophos_email_id(None, "queue_id"), None)
+        self.assertEqual(uuidutils.get_x_sophos_email_id(None, "queue_id"), None)
 
     def test_x_sophos_email_id_invalid(self):
-        self.assertEqual(xgemail_helper.get_x_sophos_email_id("Invalid UUID", "queue_id"), None)
+        self.assertEqual(uuidutils.get_x_sophos_email_id("Invalid UUID", "queue_id"), None)
 
     def test_x_sophos_email_id_valid(self):
         self.assertEqual(
-            xgemail_helper.get_x_sophos_email_id("e6592dab-ae0e-466c-86b1-c891eda752d8", "queue_id"),
+            uuidutils.get_x_sophos_email_id("e6592dab-ae0e-466c-86b1-c891eda752d8", "queue_id"),
             "e6592dab-ae0e-466c-86b1-c891eda752d8"
         )
 
     def test_x_sophos_email_id_valid_without_dash(self):
         self.assertEqual(
-            xgemail_helper.get_x_sophos_email_id("e6592dabae0e466c86b1c891eda752d8", "queue_id"),
+            uuidutils.get_x_sophos_email_id("e6592dabae0e466c86b1c891eda752d8", "queue_id"),
             "e6592dab-ae0e-466c-86b1-c891eda752d8"
         )
 
