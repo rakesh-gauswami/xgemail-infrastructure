aae8a7ff765eafb7b057bce96df6377c5dda1905
Narendra Shah <narendra.shah@sophos.com>
2020-11-30 11:31:51 +0530

    Removed ES change

--- cookbooks/sophos-cloud-xgemail/attributes/default.rb
+++ cookbooks/sophos-cloud-xgemail/attributes/default.rb
@@ -319,7 +319,7 @@ default['xgemail']['postfix_instance_data'] = {
     :instance_name => 'ed',
     :port => 25,
     # Give delivery queues extra padding because extra content may be created during processing
-    :msg_size_limit => (SUBMIT_MESSAGE_SIZE_LIMIT_BYTES + 204800 + 5242880),
+    :msg_size_limit => (SUBMIT_MESSAGE_SIZE_LIMIT_BYTES + 204800),
     :rcpt_size_limit => POSTFIX_INBOUND_MAX_NO_OF_RCPT_PER_REQUEST
   },
   # encryption-submit
