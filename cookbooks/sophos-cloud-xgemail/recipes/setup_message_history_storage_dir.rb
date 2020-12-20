9c178eb72cfb2d445ce6e72c6ea1fdc11029e0b6
Saravanan Subramaniam <s.subramaniam@sophos.com>
2020-12-20 13:42:20 +0000

    create msg-history storage in extended delivery servers as well.

--- cookbooks/sophos-cloud-xgemail/recipes/setup_message_history_storage_dir.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_message_history_storage_dir.rb
@@ -12,9 +12,12 @@ MH_MAIL_INFO_STORAGE_DIR  = node['xgemail']['mh_mail_info_storage_dir']
 NODE_TYPE                 = node['xgemail']['cluster_type']
 SERVICE_USER              = node['xgemail']['sqs_message_processor_user']
 
-# create mh mail info storage directory in delivery servers
-if NODE_TYPE == 'customer-delivery' || NODE_TYPE == 'internet-delivery' || NODE_TYPE == 'encryption-delivery' || 
-    NODE_TYPE == 'risky-delivery' || NODE_TYPE == 'warmup-delivery' || NODE_TYPE == 'beta-delivery' || NODE_TYPE == 'delta-delivery'
+# create mh mail info storage directory in all delivery servers & extended delivery servers
+if NODE_TYPE == 'customer-delivery' || NODE_TYPE == 'internet-delivery' || NODE_TYPE == 'risky-delivery' ||
+    NODE_TYPE == 'warmup-delivery' || NODE_TYPE == 'beta-delivery' || NODE_TYPE == 'delta-delivery' ||
+    NODE_TYPE == 'customer-xdelivery' || NODE_TYPE == 'internet-xdelivery' || NODE_TYPE == 'risky-xdelivery' ||
+    NODE_TYPE == 'warmup-xdelivery' || NODE_TYPE == 'beta-xdelivery' || NODE_TYPE == 'delta-xdelivery'||
+    NODE_TYPE == 'encryption-delivery'
 
     directory MH_MAIL_INFO_STORAGE_DIR do
       mode '0777'
