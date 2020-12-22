274c0910d83d61a5f24e39504e20e8d7a0d92c76
Balakrishnan Rajkumar <B.Rajkumar@sophos.com>
2020-12-22 15:56:19 +0530

    Setting ownership of ld properties file.

--- cookbooks/sophos-cloud-xgemail/recipes/install_jilter_encryption.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/install_jilter_encryption.rb
@@ -98,6 +98,13 @@ directory MSG_HISTORY_EVENT_DIR do
   recursive true
 end
 
+# Give ownership to the jilter service user
+file "#{JILTER_CONF_DIR}/launch_darkly_#{ACCOUNT}.properties" do
+  owner SERVICE_USER
+  group SERVICE_USER
+  action :touch
+end
+
 # Create the Jilter service
 template 'xgemail.jilter.service.sh' do
   path JILTER_SCRIPT_PATH
