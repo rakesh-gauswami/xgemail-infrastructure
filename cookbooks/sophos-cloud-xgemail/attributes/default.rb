7556856a53c6a76bcca3bf840a1b7e5fa8e72985
Balakrishnan Rajkumar <B.Rajkumar@sophos.com>
2021-02-06 15:10:06 +0530

    Using variable instead of hardcoding path

--- cookbooks/sophos-cloud-xgemail/attributes/default.rb
+++ cookbooks/sophos-cloud-xgemail/attributes/default.rb
@@ -266,7 +266,7 @@ default['xgemail']['sysctl_tcp_tw_reuse'] = 1
 default['xgemail']['sysctl_tcp_window_scaling'] = 1
 
 default['xgemail']['delivery_multithread_enabled_s3_path'] = 'config/delivery/multi.thread.enabled.GLOBAL'
-default['xgemail']['delivery_multithread_enabled_file_path'] = '/opt/sophos/xgemail/config/delivery.multithread.enabled'
+default['xgemail']['delivery_multithread_enabled_file_path'] = "#{XGEMAIL_FILES_DIR}/config/delivery.multithread.enabled"
 default['xgemail']['delivery_jilter_enabled_s3_path'] = 'config/delivery/jilter.enabled.GLOBAL'
 
 ## Postfix configuration
