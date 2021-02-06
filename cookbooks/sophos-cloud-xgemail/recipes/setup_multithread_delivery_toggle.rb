827f9e3128e65ff21ad15c73cfb840abe643d173
Kiran Pinna <Kiran.Pinna@Sophos.com>
2021-02-06 10:13:17 -0500

    Merge pull request #731 in EMAIL/xgemail-infrastructure from feature/XGE-17682 to develop
    * commit '7556856a53c6a76bcca3bf840a1b7e5fa8e72985':
      Using variable instead of hardcoding path
      Setting thread count to 5 for all servers
      Fixed issue with path
      Setting default thread count to 5
      Added permission to customer delivery for creating, deleting toggle files in policy bucket
      Fixed syntax error
      Fixed issue with number of threads being started
      Handled review comments.
      Fixed issue with variable name
      changed order in monit; added jllter first and then consumer script.
      added missing variables
      fix configure number of consumer threads
      fixed typo
      added missing NODE_TYPE
      updated missing AWS_REGION
      udpated AWS_REGION and POLICY_BUCKET_NAME variables
      XGE-18665: Flag management for JIlter and Consumer Multithread in Delivery Server
      Fixed issues from testing
      Setting no. of threads to 2
      Making consumer multi-threaded.

---
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_multithread_delivery_toggle.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: sophos-cloud-xgemail
+# Recipe:: setup_multithread_delivery_toggle
+#
+# Copyright 2021, Sophos
+#
+# All rights reserved - Do Not Redistribute
+#
+# This recipe configure the delivery multithread toggle script in all frontline delivery servers (excluding x-delivery servers)
+#
+
+XGEMAIL_FILES_DIR   = node['xgemail']['xgemail_files_dir']
+XGEMAIL_UTILS_DIR   = node['xgemail']['xgemail_utils_files_dir']
+XGEMAIL_CONFIG_DIR = "#{XGEMAIL_FILES_DIR}/config"
+AWS_REGION         = node['sophos_cloud']['region']
+POLICY_BUCKET_NAME = node['xgemail']['xgemail_policy_bucket_name']
+DELIVERY_MULTITHREAD_ENABLED_FILE_PATH = node['xgemail']['delivery_multithread_enabled_file_path']
+DELIVERY_MULTITHREAD_ENABLED_S3_PATH = node['xgemail']['delivery_multithread_enabled_s3_path']
+
+TOGGLE_SCRIPT_PATH = "#{XGEMAIL_FILES_DIR}/multithread-delivery-toggle"
+
+directory XGEMAIL_CONFIG_DIR do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  recursive true
+end
+
+directory TOGGLE_SCRIPT_PATH do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  recursive true
+end
+
+template "#{TOGGLE_SCRIPT_PATH}/multithread-delivery-toggle.sh" do
+  source "multithread-delivery-toggle.sh.erb"
+  mode '0750'
+  owner 'root'
+  group 'root'
+  variables(
+    :aws_region => AWS_REGION,
+    :delivery_multithread_enabled_file_path => DELIVERY_MULTITHREAD_ENABLED_FILE_PATH,
+    :delivery_multithread_enabled_s3_path => DELIVERY_MULTITHREAD_ENABLED_S3_PATH,
+    :policy_bucket => POLICY_BUCKET_NAME,
+    :xgemail_utils_dir => XGEMAIL_UTILS_DIR
+  )
+end
