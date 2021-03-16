2e16e81e58004e9b4c35704cf7628f370d22775c
mikedellacava <mike.dellacava@sophos.com>
2021-03-16 19:52:01 -0400

    MD-2251 modify rsyslog conf fix ruby

--- cookbooks/sophos-cloud-fluentd/recipes/install.rb
+++ cookbooks/sophos-cloud-fluentd/recipes/install.rb
@@ -194,7 +194,7 @@ end
 ruby_block 'edit rsyslog.conf' do
   block do
     file = Chef::Util::FileEdit.new('/etc/rsyslog.conf')
-    file.search_file_replace_line(/$OmitLocalLogging/, '#$OmitLocalLogging')
+    file.search_file_replace_line(/$OmitLocalLogging on/, '$OmitLocalLogging off')
     file.write_file
   end
 end
