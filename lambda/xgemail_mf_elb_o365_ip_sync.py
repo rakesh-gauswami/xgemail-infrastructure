688752fd8bc3ef50e15ed31f97082ca32fdce9da
mikedellacava <mike.dellacava@sophos.com>
2021-06-21 18:18:20 -0400

    MD-2535 basic lambda to set ingress on security group

--- lambda/xgemail_mf_elb_o365_ip_sync.py
+++ lambda/xgemail_mf_elb_o365_ip_sync.py
@@ -12,6 +12,7 @@ respective owners.
 
 import boto3
 import logging
+import os
 from botocore.exceptions import ClientError
 
 print('Loading function')
@@ -22,24 +23,21 @@ logger.setLevel(logging.INFO)
 region = os.environ['AWS_REGION']
 account = os.environ['ACCOUNT']
 
-ec2 = boto3.client('ec2')
-
-try:
-    response = ec2.describe_security_groups(GroupIds=['sg-0f9ec72cb03a3cdd8'])
-    print(response)
-    security_group_id = response['GroupId']
-    data = ec2.authorize_security_group_ingress(
-        GroupId=security_group_id,
-        IpPermissions=[
-            {'IpProtocol': 'tcp',
-             'FromPort': 25,
-             'ToPort': 25,
-             'IpRanges': [{'CidrIp': '73.38.163.251/32'}]},
-            {'IpProtocol': 'tcp',
-             'FromPort': 587,
-             'ToPort': 587,
-             'IpRanges': [{'CidrIp': '73.38.163.251/32'}]}
-        ])
-    print('Ingress Successfully Set %s' % data)
-except ClientError as e:
-    print(e)
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2')
+    try:
+        data = ec2.authorize_security_group_ingress(
+            GroupId='sg-0f9ec72cb03a3cdd8',
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                 'FromPort': 25,
+                 'ToPort': 25,
+                 'IpRanges': [{'CidrIp': '73.38.163.251/32'}]},
+                {'IpProtocol': 'tcp',
+                 'FromPort': 587,
+                 'ToPort': 587,
+                 'IpRanges': [{'CidrIp': '73.38.163.251/32'}]}
+            ])
+        print('Ingress Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
