4c1d0971ef9164e29a565e916686fcb9ed1a7e3d
Dipen Adroja <dipen.adroja@sophos.com>
2021-07-12 08:56:22 +0530

    Removed default transport cron

--- cookbooks/sophos-cloud-xgemail/recipes/configure-customer-delivery-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-customer-delivery-queue.rb
@@ -113,12 +113,6 @@ if NODE_TYPE == 'mf-inbound-delivery'
 end
 
 include_recipe 'sophos-cloud-xgemail::configure-bounce-message-customer-delivery-queue'
-
-# TODO XGE-8891
-# Once we're fully cut over to push policy, remove the call
-# to the setup_customer_delivery_transport_updater_cron recipe below
-#
-include_recipe 'sophos-cloud-xgemail::setup_customer_delivery_transport_updater_cron'
 include_recipe 'sophos-cloud-xgemail::setup_customer_delivery_custom_recipient_transport_updater'
 include_recipe 'sophos-cloud-xgemail::setup_transport_route_config'
 include_recipe 'sophos-cloud-xgemail::setup_xgemail_sqs_message_consumer'
