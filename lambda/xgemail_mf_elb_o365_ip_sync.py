26540f888cb22847b1bd3101114a1b55545580ae
mikedellacava <mike.dellacava@sophos.com>
2021-07-27 11:56:27 -0400

    MD-2535-1 Add description for both ports

--- lambda/xgemail_mf_elb_o365_ip_sync.py
+++ lambda/xgemail_mf_elb_o365_ip_sync.py
@@ -130,7 +130,7 @@ def add_ingress_rules(sg, ip_list):
                     {'IpProtocol': 'tcp',
                      'FromPort': 587,
                      'ToPort': 587,
-                     'IpRanges': [{'CidrIp': ip}]}
+                     'IpRanges': [{'CidrIp': ip, 'Description': 'Microsoft Office 365 Endpoint'}]}
                 ])
             logger.info("Ingress Rule " + str(ip) + " Successfully Set for SG: " + sg)
         except ClientError as e:
