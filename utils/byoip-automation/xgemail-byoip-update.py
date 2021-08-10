be937a6bf6350f78beb701a25bf420ebfeed0c4e
mikedellacava <mike.dellacava@sophos.com>
2021-08-10 17:14:50 -0400

    MD-2703 bash script and json files built

--- utils/byoip-automation/xgemail-byoip-update.py
+++ utils/byoip-automation/xgemail-byoip-update.py
@@ -4,10 +4,13 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-session = boto3.Session(profile_name='inf', region_name='eu-west-1')
-route53_client = session.client('route53')
-eip_client = session.client('ec2')
-hostedzoneid = 'ZN9712Y506MUM'
+env = ["inf", "dev", "qa", "prod"]
+regions = ["us-east-2", "us-west-2", "eu-central-1", "eu-west-1"]
+infhostedzoneid = 'ZN9712Y506MUM'
+devhostedzoneid = 'ZBKEJ4BMWTGLP'
+qahostedzoneid = 'Z3PNQYIZM8851A'
+prodhostedzoneid = 'Z1LGZDUSPJAUO4'
+hostedzoneid = infhostedzoneid
 ttl = 60
 res = ""
 
@@ -71,14 +74,19 @@ def update_byoip_tag(ipaddress, eipname):
     return response
 
 
-with open('xgemail-byoip-address-config.json', 'r') as f:
-    records_dict = json.load(f)
+for region in regions:
+    session = boto3.Session(profile_name='inf', region_name=region)
+    route53_client = session.client('route53')
+    eip_client = session.client('ec2')
 
-for byoip in records_dict:
-    print(byoip['IpAddress'])
-    res = update_byoip_dns(ipaddress=byoip['IpAddress'], dnsrecord=byoip['DnsRecord'])
-    print("Finished attempting DNS creation")
-    print(res)
-    res = update_byoip_tag(ipaddress=byoip['IpAddress'], eipname=byoip['EipName'])
-    print("Finished attempting EIP tagging")
-    print(res)
+    with open('xgemail-byoip-address-config-{0}.json'.format(region), 'r') as f:
+        records_dict = json.load(f)
+
+    for byoip in records_dict:
+        print(byoip['IpAddress'])
+        res = update_byoip_dns(ipaddress=byoip['IpAddress'], dnsrecord=byoip['DnsRecord'])
+        print("Finished attempting DNS creation")
+        print(res)
+        res = update_byoip_tag(ipaddress=byoip['IpAddress'], eipname=byoip['EipName'])
+        print("Finished attempting EIP tagging")
+        print(res)
