e2cbf39a8b94156a89ce2b23d9c2309ab7d3d05b
Balakrishnan Rajkumar <B.Rajkumar@sophos.com>
2021-08-29 19:12:29 +0530

    Reverted unwanted chagnes

--- cookbooks/sophos-cloud-xgemail/recipes/configure-postfix.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-postfix.rb
@@ -34,6 +34,7 @@ CONFIGURATION_COMMANDS_SANDBOX =
 
 SQS_MESSAGE_CONSUMER_SERVICE_NAME = node['xgemail']['sqs_message_consumer_service_name']
 
+
 service 'postfix' do
   supports :restart => true, :start => true, :stop => true, :reload => true
   action :stop
@@ -93,8 +94,8 @@ NODE_TYPE = node['xgemail']['cluster_type']
 
 if NODE_TYPE == 'customer-delivery' || NODE_TYPE == 'mf-inbound-delivery' || NODE_TYPE == 'internet-delivery' || NODE_TYPE == 'encryption-delivery' || NODE_TYPE == 'risky-delivery' || NODE_TYPE == 'warmup-delivery' || NODE_TYPE == 'beta-delivery' || NODE_TYPE == 'delta-delivery'
   MANAGED_SERVICES_IN_START_ORDER = [
-    'postfix'
-  ]
+  'postfix'
+]
 else
   if NODE_TYPE == 'internet-submit' || NODE_TYPE == 'customer-submit' || NODE_TYPE == 'encryption-submit' || NODE_TYPE == 'mf-inbound-submit'
     if ACCOUNT != 'sandbox'
