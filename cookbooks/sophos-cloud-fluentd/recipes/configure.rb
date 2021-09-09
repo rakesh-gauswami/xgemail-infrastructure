950dcfdf00d6cab2e754a4764b1ba927691e472b
Dipen Adroja <dipen.adroja@sophos.com>
2021-09-09 18:20:21 +0530

    Fixed if condition

--- cookbooks/sophos-cloud-fluentd/recipes/configure.rb
+++ cookbooks/sophos-cloud-fluentd/recipes/configure.rb
@@ -114,11 +114,10 @@ else
   NON_DELIVERY_DSN      = 'UNKNOWN'
 end
 
-if NODE_TYPE == 'mf-inbound-submit' ||
-NODE_TYPE == 'mf-outbound-delivery'
-  PRODUCT_TYPE = 'mailflow'
+if NODE_TYPE == 'mf-inbound-submit' || NODE_TYPE == 'mf-outbound-delivery'
+    PRODUCT_TYPE = 'mailflow'
 else
-  PRODUCT_TYPE = 'gateway'
+    PRODUCT_TYPE = 'gateway'
 end
 
 ### Fluentd Source Configuration Files ###
