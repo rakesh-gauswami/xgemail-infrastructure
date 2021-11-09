17b8cfcc930c7492df7197a1f18ee27a207a936a
Nelson.Han <Nelson.Han@sophos.com>
2021-11-09 13:02:15 -0500

    MD-2859 adding new outputs files

--- terraform/fsc/sdb_volume_tracker/main.tf
+++ terraform/fsc/sdb_volume_tracker/main.tf
@@ -2,5 +2,5 @@
 # SimpleDB Domain Template for Sophos MSG FSC VPC
 #
 resource "aws_simpledb_domain" "volume_tracker" {
-name = "volume-tracker"
+name = "volume_tracker"
 }
---
+++ terraform/fsc/sdb_volume_tracker/outputs-ssm-parameters.tf
@@ -0,0 +1,18 @@
+module "output_string_parameters" {
+  source = "../modules/output_string_parameters"
+  providers = {
+    aws = aws.parameters
+  }
+  parameters = [
+    {
+      name        = "/central/simpledb/domain/volume_tracker/name"
+      value       = aws_simple_db_domain.volume_tracker.name
+      description = "Volume Tracker SimpleDb Domain name"
+    },
+    {
+      name        = "/central/simpledb/domain/volume_tracker/name"
+      value       = aws_simple_db_domain.volume_tracker.id
+      description = "Volume Tracker SimpleDb Domain id"
+    }
+  ]
+}
