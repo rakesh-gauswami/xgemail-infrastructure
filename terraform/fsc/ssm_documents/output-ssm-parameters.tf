eeeaa4df99496f40d4796ee31a8cc4e422c20074
Ryan Murphy <ryan.murphy@sophos.com>
2021-11-11 13:30:22 -0500

    MD-2597 Update to ssm_document module core

--- terraform/fsc/ssm_documents/output-ssm-parameters.tf
+++ terraform/fsc/ssm_documents/output-ssm-parameters.tf
@@ -6,6 +6,13 @@ module "output_string_parameters" {
   }
 
   parameters = [
+
+    {
+      name        = "/central/ssm/document/ssm_postconf_command/name"
+      value       = aws_ssm_document.ssm_postconf_command.name
+      description = "Postconf Command SSM Document Name"
+    },
+
     {
       name        = "/central/ssm/document/ssm_postfix_service/name"
       value       = aws_ssm_document.ssm_postfix_service.name
@@ -16,6 +23,12 @@ module "output_string_parameters" {
       name        = "/central/ssm/document/ssm_update_hostname/name"
       value       = aws_ssm_document.ssm_update_hostname.name
       description = "Update Hostname SSM Document Name"
+    },
+
+    {
+      name        = "/central/ssm/document/tag_instance_automation/name"
+      value       = aws_ssm_document.tag_instance_automation.name
+      description = "Tag Instance Automation SSM Document Name"
     }
   ]
 }
