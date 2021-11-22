5eea552bebe58e1c170083abd1c7df44d4c6a6f0
Ryan Murphy <ryan.murphy@sophos.com>
2021-11-22 17:24:52 -0500

    MD-2859 Fixed reference.

--- terraform/fsc/sdb_volume_tracker/outputs-ssm-parameters.tf
+++ terraform/fsc/sdb_volume_tracker/outputs-ssm-parameters.tf
@@ -6,12 +6,12 @@ module "output_string_parameters" {
   parameters = [
     {
       name        = "/central/simpledb/domain/volume_tracker/name"
-      value       = aws_simple_db_domain.volume_tracker.name
+      value       = aws_simpledb_domain.volume_tracker.name
       description = "Volume Tracker SimpleDb Domain name"
     },
     {
       name        = "/central/simpledb/domain/volume_tracker/id"
-      value       = aws_simple_db_domain.volume_tracker.id
+      value       = aws_simpledb_domain.volume_tracker.id
       description = "Volume Tracker SimpleDb Domain id"
     }
   ]
--- terraform/fsc/sdb_volume_tracker/outputs.tf
+++ terraform/fsc/sdb_volume_tracker/outputs.tf
@@ -3,8 +3,8 @@
 ##################################################################################
 
 output "volume_tracker_name" {
-  value = aws_simple_db_domain.volume_tracker.name
+  value = aws_simpledb_domain.volume_tracker.name
 }
 output "volume_tracker_id" {
-  value = aws_simple_db_domain.volume_tracker.id
+  value = aws_simpledb_domain.volume_tracker.id
 }
