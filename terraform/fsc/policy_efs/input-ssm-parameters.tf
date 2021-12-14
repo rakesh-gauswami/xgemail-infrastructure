4ed3e0ee3efe5b6a961a7dcbb60cd8507b0a6d7a
mikedellacava <mike.dellacava@sophos.com>
2021-12-14 15:57:07 -0500

    MD-3024 Fixing some parameters

--- terraform/fsc/policy_efs/input-ssm-parameters.tf
+++ terraform/fsc/policy_efs/input-ssm-parameters.tf
@@ -1,11 +1,16 @@
 locals {
   input_param_deployment_environment = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
-  input_param_security_groups        = split(",", nonsensitive(data.aws_ssm_parameter.security_groups.value))
-  input_param_subnet_ids             = nonsensitive(data.aws_ssm_parameter.subnet_ids.value)
+  input_param_subnet_ids             = split(",", nonsensitive(data.aws_ssm_parameter.subnet_ids.value))
+  input_param_security_groups        = nonsensitive(data.aws_ssm_parameter.security_groups.value)
 }
 
 data "aws_ssm_parameter" "deployment_environment" {
   name     = "/central/account/deployment-environment"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "subnet_ids" {
+  name     = "/central/vpc/private-subnet-ids"
   provider = aws.parameters
 }
 
@@ -14,7 +19,3 @@ data "aws_ssm_parameter" "security_groups" {
   provider = aws.parameters
 }
 
-data "aws_ssm_parameter" "subnet_ids" {
-  name     = "/central/vpc/private-subnet-ids"
-  provider = aws.parameters
-}
