e97eea3ee4f91ff3820fc1656fb28fb37f0006e7
mikedellacava <mike.dellacava@sophos.com>
2021-12-14 15:58:20 -0500

    MD-3024 Fixing some parameters

--- terraform/fsc/policy_efs/efs-mount-target.tf
+++ terraform/fsc/policy_efs/efs-mount-target.tf
@@ -1,17 +1,17 @@
 resource "aws_efs_mount_target" "mount-target-subnet-a" {
   file_system_id  = aws_efs_file_system.xgemail-policy-efs-volume.id
-  subnet_id       = element([local.input_param_subnet_ids], 0)
+  subnet_id       = element(local.input_param_subnet_ids, 0)
   security_groups = [local.input_param_security_groups]
 }
 
 resource "aws_efs_mount_target" "mount-target-subnet-b" {
   file_system_id  = aws_efs_file_system.xgemail-policy-efs-volume.id
-  subnet_id       = element([local.input_param_subnet_ids], 1)
+  subnet_id       = element(local.input_param_subnet_ids, 1)
   security_groups = [local.input_param_security_groups]
 }
 
 resource "aws_efs_mount_target" "mount-target-subnet-c" {
   file_system_id  = aws_efs_file_system.xgemail-policy-efs-volume.id
-  subnet_id       = element([local.input_param_subnet_ids], 2)
+  subnet_id       = element(local.input_param_subnet_ids, 2)
   security_groups = [local.input_param_security_groups]
 }
