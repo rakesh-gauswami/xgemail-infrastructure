0b86bfe674aa02734cd1a55136e16a19f385bfbf
mikedellacava <mike.dellacava@sophos.com>
2021-12-20 16:45:06 -0500

    MD-2765 Initial commit to get this started.

--- terraform/fsc/postfix/internet_delivery/input-ssm-parameters.tf
+++ terraform/fsc/postfix/internet_delivery/input-ssm-parameters.tf
@@ -3,7 +3,7 @@ locals {
   input_param_account_type               = nonsensitive(data.aws_ssm_parameter.account_type.value)
   input_param_deployment_environment     = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
   input_param_primary_region             = nonsensitive(data.aws_ssm_parameter.primary_region.value)
-  input_param_public_subnet_ids         = nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value)
+  input_param_public_subnet_ids          = nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value)
   input_param_sg_base_id                 = nonsensitive(data.aws_ssm_parameter.sg_base_id.value)
   input_param_sg_logicmonitor_id         = nonsensitive(data.aws_ssm_parameter.sg_logicmonitor_id.value)
   input_param_vpc_id                     = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
