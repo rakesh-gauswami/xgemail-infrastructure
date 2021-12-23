fdfa62c550b5cef5e3d9549c9b5dfc9f70e9485a
mikedellacava <mike.dellacava@sophos.com>
2021-12-23 13:57:56 -0500

    MD-3195 Lookup SG IDs with SSM at runtime

--- terraform/fsc/mf_elb_o365_ip_sync/input-ssm-parameters.tf
+++ terraform/fsc/mf_elb_o365_ip_sync/input-ssm-parameters.tf
@@ -2,8 +2,6 @@ locals {
   input_param_account_type            = nonsensitive(data.aws_ssm_parameter.account_type.value)
   input_param_deployment_environment  = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
   input_param_primary_region          = nonsensitive(data.aws_ssm_parameter.primary_region.value)
-  input_param_mf_is_security_group_id = nonsensitive(data.aws_ssm_parameter.mf_is_security_group_id.value)
-  input_param_mf_os_security_group_id = nonsensitive(data.aws_ssm_parameter.mf_os_security_group_id.value)
 }
 
 data "aws_ssm_parameter" "account_type" {
@@ -13,16 +11,6 @@ data "aws_ssm_parameter" "account_type" {
 
 data "aws_ssm_parameter" "deployment_environment" {
   name     = "/central/account/deployment-environment"
-  provider = aws.parameters
-}
-
-data "aws_ssm_parameter" "mf_os_security_group_id" {
-  name     = "/central/sg/mf/outbound/submit/lb/id"
-  provider = aws.parameters
-}
-
-data "aws_ssm_parameter" "mf_is_security_group_id" {
-  name     = "/central/sg/mf/inbound/submit/lb/id"
   provider = aws.parameters
 }
 
