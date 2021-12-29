d528f6c812c51eceff07ffe8c3b44adddcf4ec21
abhijit.sharma <abhijit.sharma@sophos.com>
2021-12-29 21:01:06 +0530

    MD-2860 working on block all public access

--- terraform/fsc/s3_buckets/cloud-3rdparty.tf
+++ terraform/fsc/s3_buckets/cloud-3rdparty.tf
@@ -41,6 +41,15 @@ module "cloud_3rdparty_bucket" {
   ]
 }
 
+resource "aws_s3_bucket_public_access_block" "cloud_3rdparty_bucket_public_access" {
+  bucket = module.cloud_3rdparty_bucket.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = false
+}
+
 data "aws_iam_policy_document" "cloud_3rdparty_bucket_read_policy" {
   policy_id = "cloud_3rdparty_bucket_read_policy"
 
