953bb0203f4dee53874592ea47c7f2ba989ea705
abhijit.sharma <abhijit.sharma@sophos.com>
2021-12-30 16:17:11 +0530

    MD-2860 Disable KMS and removed the code from output parameter

--- terraform/fsc/s3_buckets/output-ssm-parameters.tf
+++ terraform/fsc/s3_buckets/output-ssm-parameters.tf
@@ -37,12 +37,6 @@ module "output_string_parameters" {
     },
 
     {
-      name        = "/central/iam/policies/cloud-3rdparty-s3-kms-policy/arn"
-      value       = aws_iam_policy.cloud_3rdparty_s3_kms_policy.arn
-      description = "Cloud 3rdparty S3 bucket KMS policy ARN"
-    },
-
-    {
       name        = "/central/iam/policies/cloud-3rdparty-bucket-delete/arn"
       value       = aws_iam_policy.cloud_3rdparty_bucket_delete_policy.arn
       description = "Cloud 3rdparty S3 bucket delete policy ARN"
@@ -97,12 +91,6 @@ module "output_string_parameters" {
     },
 
     {
-      name        = "/central/iam/policies/cloud-lambda-s3-kms-policy/arn"
-      value       = aws_iam_policy.cloud_lambda_s3_kms_policy.arn
-      description = "Cloud Lambda S3 bucket KMS policy ARN"
-    },
-
-    {
       name        = "/central/iam/policies/cloud-lambda-bucket-delete/arn"
       value       = aws_iam_policy.cloud_lambda_bucket_delete_policy.arn
       description = "Cloud Lambda S3 bucket delete policy ARN"
@@ -124,12 +112,6 @@ module "output_string_parameters" {
       name        = "/central/iam/policies/cloud-lambda-bucket-write/arn"
       value       = aws_iam_policy.cloud_lambda_bucket_write_policy.arn
       description = "Cloud Lambda S3 bucket write policy ARN"
-    },
-
-    {
-      name        = "/central/iam/policies/cloud-logs-s3-kms-policy/arn"
-      value       = aws_iam_policy.cloud_logs_s3_kms_policy.arn
-      description = "Cloud Logs S3 bucket KMS policy ARN"
     },
 
     {
