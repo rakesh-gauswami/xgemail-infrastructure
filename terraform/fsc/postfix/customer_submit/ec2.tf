ab92bbe6a18e8aa39b95471ac870b07de94cc623
Ryan Murphy <ryan.murphy@sophos.com>
2021-12-29 23:09:06 -0500

    MD-2763 Updates.

--- terraform/fsc/postfix/customer_submit/ec2.tf
+++ terraform/fsc/postfix/customer_submit/ec2.tf
@@ -7,12 +7,13 @@ locals {
   DEFAULT_AS_MAX_SIZE                   = 1
   DEFAULT_AS_MIN_SERVICE                = 1
   DEFAULT_AS_MAX_BATCH_SIZE             = 1
-  DEFAULT_AS_CRON_SCALE_IN              = "00 02 * * 1-5"
-  DEFAULT_AS_CRON_SCALE_OUT             = "30 14 * * 1-5"
+  DEFAULT_AS_CRON_SCALE_IN              = "0 1 * * 6"
+  DEFAULT_AS_CRON_SCALE_OUT             = "0 4 * * 1"
   DEFAULT_AS_HEALTH_CHECK_GRACE_PERIOD  = 2400
   DEFAULT_AS_POLICY_TARGET_VALUE        = 90
   DEFAULT_AS_ON_HOUR_DESIRED            = 2
   DEFAULT_AS_SCALE_IN_OUT_WEEKDAYS      = false
+  DEFAULT_AS_SCALE_IN_ON_WEEKENDS       = false
   DEFAULT_INSTANCE_SIZE                 = "t2.small"
   DEFAULT_INSTANCE_COUNT                = 1
   DEFAULT_VOLUME_SIZE_GIBS              = 35
@@ -83,6 +84,13 @@ locals {
   }
 
   AS_SCALE_IN_OUT_WEEKDAYS_BY_ENVIRONMENT = {
+    inf  = 1
+    dev  = 1
+    qa   = 1
+    prod = 1
+  }
+
+  AS_SCALE_IN_ON_WEEKENDS_BY_ENVIRONMENT = {
     inf  = 1
     dev  = 1
     qa   = 1
@@ -197,6 +205,12 @@ locals {
     local.DEFAULT_AS_SCALE_IN_OUT_WEEKDAYS
   )
 
+  as_scale_in_on_weekends = lookup(
+    local.AS_SCALE_IN_ON_WEEKENDS_BY_ENVIRONMENT,
+    local.input_param_deployment_environment,
+    local.DEFAULT_AS_SCALE_IN_ON_WEEKENDS
+  )
+
   instance_size = lookup(
     local.INSTANCE_SIZE_BY_ENVIRONMENT,
     local.input_param_deployment_environment,
@@ -263,11 +277,11 @@ resource "aws_cloudformation_stack" "cloudformation_stack" {
     MessageHistoryEventsTopicArn      = var.msg_history_events_sns_topic
     PolicyTargetValue                 = local.as_policy_target_value
     S3CookbookRepositoryURL           = "//${local.input_param_cloud_templates_bucket_name}/${var.build_branch}/cookbooks.enc"
-    ScaleInOnWeekends                 = local.weekend_scale_down
-    ScaleInCron                       = local.cron_scale_down
-    ScaleOutCron                      = local.cron_scale_up
-    ScheduledASOnHourDesiredCapacity  = local.on_hour_desired
-    ScaleInAndOutOnWeekdays           = local.scale_in_out_weekdays
+    ScaleInOnWeekends                 = local.as_scale_in_on_weekends
+    ScaleInCron                       = local.as_cron_scale_in
+    ScaleOutCron                      = local.as_cron_scale_out
+    ScheduledASOnHourDesiredCapacity  = local.as_on_hour_desired
+    ScaleInAndOutOnWeekdays           = local.as_scale_in_out_weekdays
     ScaleInOnWeekdaysCron             = local.cron_scale_in
     ScaleOutOnWeekdaysCron            = local.cron_scale_out
     SecurityGroups                    = [local.input_param_sg_base_id, aws_security_group.security_group_ec2]
@@ -278,7 +292,7 @@ resource "aws_cloudformation_stack" "cloudformation_stack" {
     VpcZoneIdentifiers                = [local.input_param_public_subnet_ids]
     VpcName                           = "email"
     XgemailBucketName                 = var.customer_submit_bucket
-    XgemailMinSizeDataGB              = local.volume_size_data_gb
+    XgemailMinSizeDataGB              = local.volume_size_gibs
     XgemailMsgHistoryBucketName       = var.msg_history_bucket
     XgemailMsgHistoryMsBucketName     = var.msg_history_ms_bucket
     XgemailMsgHistoryQueueUrl         = var.msg_history_sqs_queue
