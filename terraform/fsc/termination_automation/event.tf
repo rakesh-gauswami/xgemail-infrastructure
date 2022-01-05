f7dd202a77945433ef0c03a0506e143596eaf640
Ryan Murphy <ryan.murphy@sophos.com>
2022-01-05 15:48:31 -0500

    MD-2763 convert heredoc to hcl.

--- terraform/fsc/termination_automation/event.tf
+++ terraform/fsc/termination_automation/event.tf
@@ -21,6 +21,7 @@ locals {
     }
   }
 }
+
 resource "aws_cloudwatch_event_rule" "termination_automation" {
   name        = "termination-automation"
   description = "Capture ASG Instance Termination"
@@ -43,7 +44,7 @@ resource "aws_cloudwatch_event_target" "termination_automation" {
       "lifecycle_hook_name" : "$.detail.LifecycleHookName",
       "lifecycle_action_token" : "$.detail.LifecycleActionToken",
     }
-    input_template = <<DOC
+    input_template = <<-DOC
     {"Region":[<region>],"Time":[<time>],"AutoScalingGroupName":[<autocaling_group_name>],"InstanceId":[<instance_id>],"LifecycleHookName":[<lifecycle_hook_name>],"LifecycleActionToken":[<lifecycle_action_token>]}
   DOC
   }
