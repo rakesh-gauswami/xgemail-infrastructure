ec50183c3c0063d21f98478fb0bfdf93589ed211
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-05 17:14:13 +0530

    MD-2774 added input ssm parameters

--- terraform/fsc/postfix/encryption_submit/input-ssm-parameters.tf
+++ terraform/fsc/postfix/encryption_submit/input-ssm-parameters.tf
@@ -1,14 +1,42 @@
 locals {
-  input_param_account_type           = nonsensitive(data.aws_ssm_parameter.account_type.value)
-  input_param_deployment_environment = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
-  input_param_primary_region         = nonsensitive(data.aws_ssm_parameter.primary_region.value)
-  input_param_sg_base_id             = nonsensitive(data.aws_ssm_parameter.sg_base_id.value)
-  input_param_sg_logicmonitor_id     = nonsensitive(data.aws_ssm_parameter.sg_logicmonitor_id.value)
-  input_param_vpc_id                 = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
+  input_param_account_name             = nonsensitive(data.aws_ssm_parameter.account_name.value)
+  input_param_account_type             = nonsensitive(data.aws_ssm_parameter.account_type.value)
+  input_param_autoscaling_role_arn     = nonsensitive(data.aws_ssm_parameter.autoscaling_role_arn.value)
+  input_param_availability_zones       = nonsensitive(data.aws_ssm_parameter.availability_zones.value)
+  input_param_deployment_environment   = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
+  input_param_primary_region           = nonsensitive(data.aws_ssm_parameter.primary_region.value)
+  input_param_public_subnet_ids        = split(",", nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value))
+  input_param_sg_base_id               = nonsensitive(data.aws_ssm_parameter.sg_base_id.value)
+  input_param_sg_logicmonitor_id       = nonsensitive(data.aws_ssm_parameter.sg_logicmonitor_id.value)
+  input_param_vpc_id                   = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
+  input_param_zone_fqdn                = nonsensitive(data.aws_ssm_parameter.zone_fqdn.value)
+  input_param_zone_id                  = nonsensitive(data.aws_ssm_parameter.zone_id.value)
+  input_param_alarm_topic_arn          = nonsensitive(data.aws_ssm_parameter.alarm_topic_arn.value)
+  input_param_iam_instance_profile_arn = nonsensitive(data.aws_ssm_parameter.iam_instance_profile_name.value)
+}
+
+data "aws_ssm_parameter" "account_name" {
+  name     = "/central/account/name"
+  provider = aws.parameters
 }
 
 data "aws_ssm_parameter" "account_type" {
   name     = "/central/account/type"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "alarm_topic_arn" {
+  name     = "/central/sns/alarm-topic/arn"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "autoscaling_role_arn" {
+  name     = "/central/iam/roles/autoscaling/arn"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "availability_zones" {
+  name     = "/central/vpc/availability-zones"
   provider = aws.parameters
 }
 
@@ -17,8 +45,18 @@ data "aws_ssm_parameter" "deployment_environment" {
   provider = aws.parameters
 }
 
+data "aws_ssm_parameter" "iam_instance_profile_name" {
+  name     = "/central/iam/profiles/${local.instance_type}-instance/name"
+  provider = aws.parameters
+}
+
 data "aws_ssm_parameter" "primary_region" {
   name     = "/central/account/primary-region"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name     = "/central/vpc/public-subnet-ids"
   provider = aws.parameters
 }
 
@@ -38,3 +76,14 @@ data "aws_ssm_parameter" "vpc_id" {
   provider = aws.parameters
   name     = "/central/vpc/id"
 }
+
+data "aws_ssm_parameter" "zone_fqdn" {
+  name     = "/central/account/dns/zone-fqdn"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "zone_id" {
+  name     = "/central/account/dns/zone-id"
+  provider = aws.parameters
+}
+
