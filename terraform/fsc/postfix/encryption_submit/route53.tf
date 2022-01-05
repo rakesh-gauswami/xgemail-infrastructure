7d42c8227deab389c2d55f1c735c578583c76969
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-05 17:15:04 +0530

    MD-2774 added route53

--- terraform/fsc/postfix/encryption_submit/route53.tf
+++ terraform/fsc/postfix/encryption_submit/route53.tf
@@ -1,3 +1,11 @@
-locals {
+data "aws_route53_zone" "hosted_zone" {
+  zone_id = local.input_param_zone_id
+}
 
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = "relay.${local.input_param_zone_fqdn}"
+  type    = "CNAME"
+  ttl     = "900"
+  records = [aws_elb.elb.dns_name]
 }
