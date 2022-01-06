889dacf4888a1cd6d79509eb0f4ef8e7950afed7
Swaroop Mallenahalli <Swaroop.M@CA-MN-43000.local>
2022-01-06 15:33:19 -0800

    update fsc internet-submit terraform files

--- terraform/fsc/postfix/internet_submit/sg.tf
+++ terraform/fsc/postfix/internet_submit/sg.tf
@@ -1,7 +1,19 @@
 locals {
-  cidr_block_world       = "0.0.0.0/0"
-  smtp_tcp_port          = 25
-  security_group_name_lb = "${local.instance_type}-lb"
+  cidr_block_world        = "0.0.0.0/0"
+  efs_tcp_port            = 2049
+  ntp_udp_port            = 123
+  smtp_tcp_port           = 25
+  snmp_port               = 161
+  snmp_trap_port          = 162
+  security_group_name_lb  = "${local.instance_type}-lb"
+}
+
+data "aws_security_group" "base" {
+  id = local.input_param_sg_base_id
+}
+
+data "aws_security_group" "efs_policy" {
+  id = local.input_param_sg_efs_policy_id
 }
 
 resource "aws_security_group" "security_group_lb" {
@@ -10,15 +22,6 @@ resource "aws_security_group" "security_group_lb" {
   vpc_id      = local.input_param_vpc_id
 
   tags = { Name = local.security_group_name_lb }
-}
-
-resource "aws_security_group_rule" "lb_egress_world" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.security_group_lb.id
-  cidr_blocks       = [local.cidr_block_world]
 }
 
 resource "aws_security_group" "security_group_ec2" {
@@ -30,12 +33,30 @@ resource "aws_security_group" "security_group_ec2" {
 }
 
 resource "aws_security_group_rule" "ec2_egress_world" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.security_group_ec2.id
-  cidr_blocks       = [local.cidr_block_world]
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks               = [local.cidr_block_world]
+  security_group_id        = aws_security_group.security_group_ec2.id
+}
+
+resource "aws_security_group_rule" "lb_egress_world" {
+  type                     = "egress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  cidr_blocks               = [local.cidr_block_world]
+  security_group_id        = aws_security_group.security_group_lb.id
+}
+
+resource "aws_security_group_rule" "lb_ingress_world_smtp" {
+  type                      = "ingress"
+  from_port                 = local.smtp_tcp_port
+  to_port                   = local.smtp_tcp_port
+  protocol                  = "tcp"
+  cidr_blocks               = [local.cidr_block_world]
+  security_group_id         = aws_security_group.security_group_lb.id
 }
 
 resource "aws_security_group_rule" "ec2_ingress_lb_smtp" {
@@ -45,4 +66,13 @@ resource "aws_security_group_rule" "ec2_ingress_lb_smtp" {
   protocol                 = "tcp"
   security_group_id        = aws_security_group.security_group_ec2.id
   source_security_group_id = aws_security_group.security_group_lb.id
+}
+
+resource "aws_security_group_rule" "efs_policy_ingress_tcp" {
+  type                     = "ingress"
+  from_port                = local.efs_tcp_port
+  to_port                  = local.efs_tcp_port
+  protocol                 = "tcp"
+  security_group_id        = data.aws_security_group.efs_policy.id
+  source_security_group_id = aws_security_group.security_group_ec2.id
 }
