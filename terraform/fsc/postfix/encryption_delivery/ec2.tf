f954ad899e3ef1510f7593e25f6d73912d5c124e
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-07 22:34:53 +0530

    MD-2775 remove scaling cron

--- terraform/fsc/postfix/encryption_delivery/ec2.tf
+++ terraform/fsc/postfix/encryption_delivery/ec2.tf
@@ -6,12 +6,8 @@ locals {
   DEFAULT_AS_MAX_BATCH_SIZE            = 1
   DEFAULT_AS_CRON_SCALE_DOWN           = "0 1 * * 6"
   DEFAULT_AS_CRON_SCALE_UP             = "0 4 * * 1"
-  DEFAULT_AS_CRON_SCALE_IN             = "00 02 * * 1-5"
-  DEFAULT_AS_CRON_SCALE_OUT            = "30 14 * * 1-5"
   DEFAULT_AS_HEALTH_CHECK_GRACE_PERIOD = 2400
   DEFAULT_AS_POLICY_TARGET_VALUE       = 90
-  DEFAULT_AS_ON_HOUR_DESIRED           = 2
-  DEFAULT_AS_SCALE_IN_OUT_WEEKDAYS     = false
   DEFAULT_AS_SCALE_IN_ON_WEEKENDS      = false
   DEFAULT_INSTANCE_SIZE                = "t2.small"
   DEFAULT_INSTANCE_COUNT               = 1
@@ -58,20 +54,6 @@ locals {
     prod = "0 4 * * 1"
   }
 
-  AS_CRON_SCALE_IN_BY_ENVIRONMENT = {
-    inf  = "00 02 * * 1-5"
-    dev  = "00 02 * * 1-5"
-    qa   = "00 02 * * 1-5"
-    prod = "00 02 * * 1-5"
-  }
-
-  AS_CRON_SCALE_OUT_BY_ENVIRONMENT = {
-    inf  = "30 14 * * 1-5"
-    dev  = "30 14 * * 1-5"
-    qa   = "30 14 * * 1-5"
-    prod = "30 14 * * 1-5"
-  }
-
   AS_HEALTH_CHECK_GRACE_PERIOD_BY_ENVIRONMENT = {
     inf  = 2400
     dev  = 2400
@@ -84,20 +66,6 @@ locals {
     dev  = 90
     qa   = 90
     prod = 65
-  }
-
-  AS_ON_HOUR_DESIRED_BY_ENVIRONMENT = {
-    inf  = 2
-    dev  = 2
-    qa   = 2
-    prod = 2
-  }
-
-  AS_SCALE_IN_OUT_WEEKDAYS_BY_ENVIRONMENT = {
-    inf  = false
-    dev  = false
-    qa   = false
-    prod = false
   }
 
   AS_SCALE_IN_ON_WEEKENDS_BY_ENVIRONMENT = {
@@ -184,18 +152,6 @@ locals {
     local.DEFAULT_AS_CRON_SCALE_UP
   )
 
-  as_cron_scale_in = lookup(
-    local.AS_CRON_SCALE_IN_BY_ENVIRONMENT,
-    local.input_param_deployment_environment,
-    local.DEFAULT_AS_CRON_SCALE_IN
-  )
-
-  as_cron_scale_out = lookup(
-    local.AS_CRON_SCALE_OUT_BY_ENVIRONMENT,
-    local.input_param_deployment_environment,
-    local.DEFAULT_AS_CRON_SCALE_OUT
-  )
-
   health_check_grace_period = lookup(
     local.AS_HEALTH_CHECK_GRACE_PERIOD_BY_ENVIRONMENT,
     local.input_param_deployment_environment,
@@ -206,18 +162,6 @@ locals {
     local.AS_POLICY_TARGET_VALUE_BY_ENVIRONMENT,
     local.input_param_deployment_environment,
     local.DEFAULT_AS_POLICY_TARGET_VALUE
-  )
-
-  as_on_hour_desired = lookup(
-    local.AS_ON_HOUR_DESIRED_BY_ENVIRONMENT,
-    local.input_param_deployment_environment,
-    local.DEFAULT_AS_ON_HOUR_DESIRED
-  )
-
-  as_scale_in_out_weekdays = lookup(
-    local.AS_SCALE_IN_OUT_WEEKDAYS_BY_ENVIRONMENT,
-    local.input_param_deployment_environment,
-    local.DEFAULT_AS_SCALE_IN_OUT_WEEKDAYS
   )
 
   as_scale_in_on_weekends = lookup(
@@ -291,10 +235,6 @@ resource "aws_cloudformation_stack" "cloudformation_stack" {
     ScaleInOnWeekends                = local.as_scale_in_on_weekends
     ScaleInCron                      = local.as_cron_scale_down
     ScaleOutCron                     = local.as_cron_scale_up
-    ScheduledASOnHourDesiredCapacity = local.as_on_hour_desired
-    ScaleInAndOutOnWeekdays          = local.as_scale_in_out_weekdays
-    ScaleInOnWeekdaysCron            = local.as_cron_scale_in
-    ScaleOutOnWeekdaysCron           = local.as_cron_scale_out
     SecurityGroups                   = aws_security_group.security_group_ec2.id
     SpotPrice                        = "-1"
     StationVpcId                     = var.station_vpc_id
