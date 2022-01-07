fd47c60400c65eb69ffd624688a1d266d038b5e0
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-07 18:56:04 +0530

    MD-2775 update input ssm parameters

--- terraform/fsc/postfix/encryption_delivery/input-ssm-parameters.tf
+++ terraform/fsc/postfix/encryption_delivery/input-ssm-parameters.tf
@@ -5,6 +5,9 @@ locals {
   input_param_sg_base_id                      = nonsensitive(data.aws_ssm_parameter.sg_base_id.value)
   input_param_sg_logicmonitor_id              = nonsensitive(data.aws_ssm_parameter.sg_logicmonitor_id.value)
   input_param_vpc_id                          = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
+  input_param_public_subnet_ids               = split(",", nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value))
+  input_param_zone_id                         = nonsensitive(data.aws_ssm_parameter.zone_id.value)
+  input_param_zone_fqdn                       = nonsensitive(data.aws_ssm_parameter.zone_fqdn.value)
 }
 
 data "aws_ssm_parameter" "account_type" {
@@ -22,6 +25,11 @@ data "aws_ssm_parameter" "primary_region" {
   provider = aws.parameters
 }
 
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name     = "/central/vpc/public-subnet-ids"
+  provider = aws.parameters
+}
+
 data "aws_ssm_parameter" "sg_base_id" {
   provider = aws.parameters
 
@@ -34,7 +42,17 @@ data "aws_ssm_parameter" "sg_logicmonitor_id" {
   name = "/central/sg/logicmonitor/id"
 }
 
+data "aws_ssm_parameter" "zone_fqdn" {
+  name     = "/central/account/dns/zone-fqdn"
+  provider = aws.parameters
+}
+
 data "aws_ssm_parameter" "vpc_id" {
   provider = aws.parameters
   name     = "/central/vpc/id"
+}
+
+data "aws_ssm_parameter" "zone_id" {
+  name     = "/central/account/dns/zone-id"
+  provider = aws.parameters
 }
