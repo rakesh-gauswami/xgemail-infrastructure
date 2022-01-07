73dda6cf6d867a9f39731464659d814b95e04765
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-07 23:32:19 +0530

    MD-2775 update dns record

--- terraform/fsc/postfix/encryption_delivery/route53.tf
+++ terraform/fsc/postfix/encryption_delivery/route53.tf
@@ -2,7 +2,7 @@ data "aws_route53_zone" "hosted_zone" {
   zone_id = local.input_param_zone_id
 }
 
-resource "aws_route53_record" "encryption-delivery" {
+resource "aws_route53_record" "route53" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = "${local.instance_type}-cloudemail.${local.input_param_zone_fqdn}"
   type    = "CNAME"
