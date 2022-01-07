0096c34c70c1a42167c6282e1304d78d0de2c84c
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-07 18:56:38 +0530

    MD-2775 update route53

--- terraform/fsc/postfix/encryption_delivery/route53.tf
+++ terraform/fsc/postfix/encryption_delivery/route53.tf
@@ -1,3 +1,11 @@
-locals {
+data "aws_route53_zone" "hosted_zone" {
+  zone_id = local.input_param_zone_id
+}
 
+resource "aws_route53_record" "encryption-delivery" {
+  zone_id = data.aws_route53_zone.hosted_zone.zone_id
+  name    = "encryption-delivery-cloudemail-${local.input_param_zone_fqdn}"
+  type    = "CNAME"
+  ttl     = "900"
+  records = [aws_elb.elb.dns_name]
 }
