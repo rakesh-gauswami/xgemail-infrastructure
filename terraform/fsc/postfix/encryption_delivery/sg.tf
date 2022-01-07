4ef497f24892f38822d12f77c11ae9654ae4d58e
abhijit.sharma <abhijit.sharma@sophos.com>
2022-01-07 23:10:11 +0530

    MD-2775 update allignment

--- terraform/fsc/postfix/encryption_delivery/sg.tf
+++ terraform/fsc/postfix/encryption_delivery/sg.tf
@@ -26,12 +26,12 @@ resource "aws_security_group" "security_group_ec2" {
 }
 
 resource "aws_security_group_rule" "ec2_egress_world" {
-  type                     = "egress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-  cidr_blocks               = [local.cidr_block_world]
-  security_group_id        = aws_security_group.security_group_ec2.id
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [local.cidr_block_world]
+  security_group_id = aws_security_group.security_group_ec2.id
 }
 
 resource "aws_security_group_rule" "ec2_ingress_lb_smtp" {
