4d39940c7692a03ecd21e0b3d6f56efd7c58f133
deekshita.manokaran <deekshita.manokaran@sophos.com>
2022-01-12 13:11:36 -0800

    Fluentd changes to parse postfix log and push it into SQS queue

--- cookbooks/sophos-cloud-xgemail/attributes/default.rb
+++ cookbooks/sophos-cloud-xgemail/attributes/default.rb
@@ -156,6 +156,7 @@ default['xgemail']['scan_events_sns_topic'] = "#{node['xgemail']['station_vpc_id
 
 # SQS Names
 default['xgemail']['msg_history_delivery_status_sqs'] = "#{node['xgemail']['station_vpc_id']}-Xgemail_MessageHistory_Delivery_Status"
+default['xgemail']['smtp_log_sqs'] = "#{node['xgemail']['station_vpc_id']}-Xgemail_Smtp_log"
 
 ## Policy service/poller settings
 default['xgemail']['sqs_policy_poller_visibility_timeout'] = '10'
