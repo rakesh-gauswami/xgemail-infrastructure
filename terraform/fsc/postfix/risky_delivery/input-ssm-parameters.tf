701e64e56c7dc05806cf150bdaf84419cbcd3e8b
Swaroop Mallenahalli <Swaroop.M@CA-MN-43000.local>
2022-01-12 13:10:45 -0800

    update fsc risky_delivery module terraform files

--- terraform/fsc/postfix/risky_delivery/input-ssm-parameters.tf
+++ terraform/fsc/postfix/risky_delivery/input-ssm-parameters.tf
@@ -1,22 +1,26 @@
 locals {
-  input_param_availability_zones         = nonsensitive(data.aws_ssm_parameter.availability_zones.value)
-  input_param_account_type               = nonsensitive(data.aws_ssm_parameter.account_type.value)
-  input_param_deployment_environment     = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
-  input_param_primary_region             = nonsensitive(data.aws_ssm_parameter.primary_region.value)
-  input_param_public_subnet_ids          = nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value)
-  input_param_sg_base_id                 = nonsensitive(data.aws_ssm_parameter.sg_base_id.value)
-  input_param_sg_logicmonitor_id         = nonsensitive(data.aws_ssm_parameter.sg_logicmonitor_id.value)
-  input_param_vpc_id                     = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
-  input_param_zone_fqdn                  = nonsensitive(data.aws_ssm_parameter.zone_fqdn.value)
-  input_param_zone_id                    = nonsensitive(data.aws_ssm_parameter.zone_id.value)
-  input_param_alarm_topic_arn            = nonsensitive(data.aws_ssm_parameter.alarm_topic_arn.value)
-  input_param_lifecycle_topic_arn        = nonsensitive(data.aws_ssm_parameter.lifecycle_topic_arn.value)
-  input_param_lifecycle_hook_terminating = nonsensitive(data.aws_ssm_parameter.lifecycle_topic_arn.value)
+  input_param_account_name                = nonsensitive(data.aws_ssm_parameter.account_name.value)
+  input_param_account_type                = nonsensitive(data.aws_ssm_parameter.account_type.value)
+  input_param_autoscaling_role_arn        = nonsensitive(data.aws_ssm_parameter.autoscaling_role_arn.value)
+  input_param_availability_zones          = nonsensitive(data.aws_ssm_parameter.availability_zones.value)
+  input_param_cloud_templates_bucket_name = nonsensitive(data.aws_ssm_parameter.cloud_templates_bucket_name.value)
+  input_param_deployment_environment      = nonsensitive(data.aws_ssm_parameter.deployment_environment.value)
+  input_param_parent_account_name         = nonsensitive(data.aws_ssm_parameter.parent_account_name.value)
+  input_param_primary_region              = nonsensitive(data.aws_ssm_parameter.primary_region.value)
+  input_param_public_subnet_ids           = split(",", nonsensitive(data.aws_ssm_parameter.public_subnet_ids.value))
+  input_param_vpc_id                      = nonsensitive(data.aws_ssm_parameter.vpc_id.value)
+  input_param_vpc_name                    = replace(nonsensitive(data.aws_ssm_parameter.vpc_name.value), "/-.*/", "")
+  input_param_zone_fqdn                   = nonsensitive(data.aws_ssm_parameter.zone_fqdn.value)
+  input_param_zone_id                     = nonsensitive(data.aws_ssm_parameter.zone_id.value)
+  input_param_alarm_topic_arn             = nonsensitive(data.aws_ssm_parameter.alarm_topic_arn.value)
+  input_param_lifecycle_topic_arn         = nonsensitive(data.aws_ssm_parameter.lifecycle_topic_arn.value)
+  input_param_lifecycle_hook_launching    = nonsensitive(data.aws_ssm_parameter.lifecycle_hook_launching_name.value)
+  input_param_lifecycle_hook_terminating  = nonsensitive(data.aws_ssm_parameter.lifecycle_hook_terminating_name.value)
+  input_param_iam_instance_profile_name   = nonsensitive(data.aws_ssm_parameter.iam_instance_profile_name.value)
 }
 
-
-data "aws_ssm_parameter" "availability_zones" {
-  name     = "/central/vpc/availability-zones"
+data "aws_ssm_parameter" "account_name" {
+  name     = "/central/account/name"
   provider = aws.parameters
 }
 
@@ -25,8 +29,28 @@ data "aws_ssm_parameter" "account_type" {
   provider = aws.parameters
 }
 
+data "aws_ssm_parameter" "autoscaling_role_arn" {
+  name     = "/central/iam/roles/autoscaling/arn"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "availability_zones" {
+  name     = "/central/vpc/availability-zones"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "cloud_templates_bucket_name" {
+  name     = "/central/s3/cloud-${local.input_param_account_name}-templates/name"
+  provider = aws.parameters
+}
+
 data "aws_ssm_parameter" "deployment_environment" {
   name     = "/central/account/deployment-environment"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "parent_account_name" {
+  name     = "/central/account/parent-account/name"
   provider = aws.parameters
 }
 
@@ -40,44 +64,47 @@ data "aws_ssm_parameter" "public_subnet_ids" {
   provider = aws.parameters
 }
 
-data "aws_ssm_parameter" "sg_base_id" {
-  provider = aws.parameters
-
-  name = "/central/sg/base/id"
-}
-
-data "aws_ssm_parameter" "sg_logicmonitor_id" {
-  provider = aws.parameters
-
-  name = "/central/sg/logicmonitor/id"
-}
-
 data "aws_ssm_parameter" "vpc_id" {
-  provider = aws.parameters
   name     = "/central/vpc/id"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "vpc_name" {
+  name     = "/central/vpc/name"
+  provider = aws.parameters
 }
 
 data "aws_ssm_parameter" "zone_fqdn" {
-  provider = aws.parameters
   name     = "/central/account/dns/zone-fqdn"
+  provider = aws.parameters
 }
 
 data "aws_ssm_parameter" "zone_id" {
-  provider = aws.parameters
   name     = "/central/account/dns/zone-id"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "lifecycle_hook_launching_name" {
+  name     = "/central/asg/${local.instance_type}/lifecycle-hook/launching/name"
+  provider = aws.parameters
 }
 
 data "aws_ssm_parameter" "lifecycle_hook_terminating_name" {
+  name     = "/central/asg/${local.instance_type}/lifecycle-hook/terminating/name"
   provider = aws.parameters
-  name     = "/central/asg/internet-submit/lifecycle-hook/terminating/name"
 }
 
 data "aws_ssm_parameter" "alarm_topic_arn" {
-  provider = aws.parameters
   name     = "/central/sns/alarm-topic/arn"
+  provider = aws.parameters
 }
 
 data "aws_ssm_parameter" "lifecycle_topic_arn" {
-  provider = aws.parameters
   name     = "/central/sns/lifecycle-topic/arn"
+  provider = aws.parameters
+}
+
+data "aws_ssm_parameter" "iam_instance_profile_name" {
+  name     = "/central/iam/profiles/${local.instance_type}-instance/name"
+  provider = aws.parameters
 }
