a644ce90ee020f92e074777370f3715a23f725d7
Swaroop Mallenahalli <Swaroop.M@CA-MN-43000.local>
2022-01-13 10:53:20 -0800

    update risky_xdelivery fsc terraform files

--- terraform/fsc/postfix/risky_xdelivery/variables.tf
+++ terraform/fsc/postfix/risky_xdelivery/variables.tf
@@ -1,3 +1,79 @@
+variable "build_branch" {
+  type        = string
+  description = "Bitbucket Branch"
+}
+
+variable "build_number" {
+  type        = string
+  description = "Build Number"
+}
+
+variable "build_tag" {
+  type        = string
+  description = "Build Tag"
+}
+
+variable "build_url" {
+  type        = string
+  description = "Build URL"
+}
+
+variable "firehose_msg_history_v2_stream_name" {
+  type        = string
+  description = "firehose_msg_history_v2_stream_name"
+}
+
+variable "message_history_bucket" {
+  type        = string
+  description = "msg_history_bucket"
+}
+
+variable "message_history_ms_bucket" {
+  type        = string
+  description = "msg_history_ms_bucket"
+}
+
+variable "message_history_dynamodb_table_name" {
+  type        = string
+  description = "msg_history_dynamodb_table_name"
+}
+
+variable "message_history_status_queue" {
+  type        = string
+  description = "message_history_status_queue"
+}
+
+variable "message_history_status_topic" {
+  type        = string
+  description = "message_history_status_topic"
+}
+
+variable "message_history_sqs_queue" {
+  type        = string
+  description = "msg_history_sqs_queue"
+}
+
+
+variable "notifier_request_sqs_queue" {
+  type        = string
+  description = "notifier_request_sqs_queue"
+}
+
+variable "policy_bucket" {
+  type        = string
+  description = "policy_bucket"
+}
+
+variable "station_vpc_id" {
+  type        = string
+  description = "station_vpc_id"
+}
+
+variable "station_name" {
+  type        = string
+  description = "station_name"
+}
+
 variable "tag_origin" {
   type = string
   # No default, set by tf-fsc.sh
