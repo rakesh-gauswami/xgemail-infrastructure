490887dca0b6ed5181cb1683939f6bdbd4b5b9b9
kirtimaya.mohanty <kirtimaya.mohanty@sophos.com>
2022-01-24 20:33:39 +0530

    Updated the route53 configurations

--- terraform/fsc/postfix/mf_inbound_submit/route53.tf
+++ terraform/fsc/postfix/mf_inbound_submit/route53.tf
@@ -2,18 +2,11 @@ data "aws_route53_zone" "hosted_zone" {
   zone_id = local.input_param_zone_id
 }
 
-resource "aws_route53_record" "mx_01" {
+resource "aws_route53_record" "route53_record" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "mx-01.${local.input_param_zone_fqdn}"
+  name    = "${local.instance_type}.${local.input_param_zone_fqdn}"
   type    = "CNAME"
   ttl     = "900"
   records = [aws_elb.elb.dns_name]
 }
 
-resource "aws_route53_record" "mx_02" {
-  zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "mx-02.${local.input_param_zone_fqdn}"
-  type    = "CNAME"
-  ttl     = "900"
-  records = [aws_elb.elb.dns_name]
-}
