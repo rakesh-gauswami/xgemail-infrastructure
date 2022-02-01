1f1221fdcb9d0eac2f9b43ac516be2a3fdce075a
Ryan Murphy <ryan.murphy@sophos.com>
2022-02-01 12:15:28 -0500

    MD-3301 Added journalctl to deployment logs.

--- utils/common/upload_deployment_logs.py
+++ utils/common/upload_deployment_logs.py
@@ -70,6 +70,7 @@ DEFAULT_LOG_COMMANDS = {
     "mount.out":        "/bin/mount -l",
     "netstat-r.out":    "/bin/netstat -rn",
     "top.out":          "/usr/bin/top -b -n 1",
+    "journalctl.out":   "/bin/journalctl -xe",
     "uname.out":        "/bin/uname -a",
 }
 
