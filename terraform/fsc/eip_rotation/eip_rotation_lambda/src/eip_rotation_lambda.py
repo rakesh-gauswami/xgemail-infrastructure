f07a2aa6b244572d906c7a6454fbd3bcaca38911
mikedellacava <mike.dellacava@sophos.com>
2022-02-03 11:39:32 -0500

    MD-3343 Setting up multi eip on all delivery and xdelivery instances

--- terraform/fsc/eip_rotation/eip_rotation_lambda/src/eip_rotation_lambda.py
+++ terraform/fsc/eip_rotation/eip_rotation_lambda/src/eip_rotation_lambda.py
@@ -174,19 +174,8 @@ def get_instances_by_name():
             {
                 'Name': 'tag:Name',
                 'Values': [
-                    'CloudEmail:internet-delivery:*',
-                    'CloudEmail:customer-delivery:*',
-                    'CloudEmail:mf-inbound-delivery:*',
-                    'CloudEmail:mf-outbound-delivery:*',
-                    'CloudEmail:mf-inbound-xdelivery:*',
-                    'CloudEmail:mf-outbound-xdelivery:*',
-                    'CloudEmail:internet-xdelivery:*',
-                    'CloudEmail:risky-delivery:*',
-                    'CloudEmail:risky-xdelivery:*',
                     'CloudEmail:beta-delivery:*',
-                    'CloudEmail:beta-xdelivery:*',
-                    'CloudEmail:delta-delivery:*',
-                    'CloudEmail:delta-xdelivery:*'
+                    'CloudEmail:beta-xdelivery:*'
                 ]
             }
         ]
