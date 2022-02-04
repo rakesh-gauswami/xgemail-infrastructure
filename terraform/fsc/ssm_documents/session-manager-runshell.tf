63e1b2c6c3be9769e74275d9a7fa179631e1d40e
Ryan Murphy <ryan.murphy@sophos.com>
2022-02-03 22:04:18 -0500

    MD-3301 Updated ssm doc syntax.

--- terraform/fsc/ssm_documents/session-manager-runshell.tf
+++ terraform/fsc/ssm_documents/session-manager-runshell.tf
@@ -18,7 +18,7 @@ locals {
       "runAsDefaultUser": "ec2-user",
       "shellProfile": {
         "windows": "",
-        "linux": "/bin/bash\nsudo -i\nexport INSTANCE_ID=$(curl -fs http://169.254.169.254/latest/meta-data/instance-id)\nexport REGION=$(curl -fs http://169.254.169.254/latest/meta-data/placement/region)\nexport APPLICATION=$(aws ec2 describe-tags --region ${REGION} --filters \"Name=resource-id,Values=${INSTANCE_ID}\" | jq -r '.Tags[] | select(.Key == \"Application\").Value')\nPS1=\"\\[\\033[01;32m\\]\\u\\[\\033[01;33m\\]@\\[\\033[01;35m\\]\\$APPLICATION\\[\\033[01;36m\\]:\\[\\033[01;34m\\]\\w\\[\\033[01;31m\\]\\$\\[\\033[0m\\] \"\nalias cheflog='less +F /var/log/chef-client.instance.install.service.log'\nalias cfninitlog='less +F /var/log/cfn-init.log'\nalias tdlog='less +F /var/log/td-agent/td-agent.log'\nclear"
+        "linux": "/bin/bash\nsudo -i\nexport AWS_INSTANCE_ID=$(curl -fs http://169.254.169.254/latest/meta-data/instance-id)\nexport REGION=$(curl -fs http://169.254.169.254/latest/meta-data/placement/region)\nexport APPLICATION=$(aws ec2 describe-tags --region $REGION --filters \"Name=resource-id,Values=$AWS_INSTANCE_ID\" | jq -r '.Tags[] | select(.Key == \"Application\").Value')\nPS1=\"\\[\\033[01;32m\\]\\u\\[\\033[01;33m\\]@\\[\\033[01;35m\\]\\$APPLICATION\\[\\033[01;36m\\]:\\[\\033[01;34m\\]\\w\\[\\033[01;31m\\]\\$\\[\\033[0m\\] \"\nalias cheflog='less +F /var/log/chef-client.instance.install.service.log'\nalias cfninitlog='less +F /var/log/cfn-init.log'\nalias tdlog='less +F /var/log/td-agent/td-agent.log'\nclear"
       }
     }
   }
