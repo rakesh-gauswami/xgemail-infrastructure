0fcd6faefd7e86fe8eee4fe9bb30caedd45ffb70
mikedellacava <mike.dellacava@sophos.com>
2022-02-08 01:42:49 -0500

    MD-3366 missing delta-delivery changes

--- terraform/fsc/postfix/delta_delivery/route53.tf
+++ terraform/fsc/postfix/delta_delivery/route53.tf
@@ -2,10 +2,10 @@ data "aws_route53_zone" "hosted_zone" {
   zone_id = local.input_param_zone_id
 }
 
-resource "aws_route53_record" "internet_delivery" {
+resource "aws_route53_record" "route53_record" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
-  name    = "delta.${local.input_param_zone_fqdn}"
-  type    = "A"
+  name    = "${local.instance_type}.${local.input_param_zone_fqdn}"
+  type    = "CNAME"
   ttl     = "900"
   records = [aws_elb.elb.dns_name]
 }
