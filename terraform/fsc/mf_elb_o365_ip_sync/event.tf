eb9bfe7d0d1dd55aa38061c9468d1f3d9772ed79
mikedellacava <mike.dellacava@sophos.com>
2022-03-04 13:42:13 -0500

    MD-3486 remove vars for mf elb sg ip sync so always enabled

--- terraform/fsc/mf_elb_o365_ip_sync/event.tf
+++ terraform/fsc/mf_elb_o365_ip_sync/event.tf
@@ -2,8 +2,8 @@ resource "aws_cloudwatch_event_rule" "mf_elb_o365_ip_sync_scheduled_event_rule" 
   name        = "mf-elb-o365-ip-sync-scheduled-event-rule"
   description = "Scheduled Cloudwatch Event for Mf Elb O365 Ip Sync"
 
-  schedule_expression = var.mf_elb_o365_ip_sync_schedule
-  is_enabled          = var.mf_elb_o365_ip_sync_schedule_enabled
+  schedule_expression = "cron(0 6 * * ? *)"
+  is_enabled          = true
 }
 
 resource "aws_cloudwatch_event_target" "mf_elb_o365_ip_sync_scheduled_event_target" {
--- terraform/fsc/mf_elb_o365_ip_sync/variables.tf
+++ terraform/fsc/mf_elb_o365_ip_sync/variables.tf
@@ -1,14 +1,3 @@
-variable "mf_elb_o365_ip_sync_schedule" {
-  description = "The schedule expression for mf elb o365 ip sync event rule"
-  default     = "cron(0 10 * * ? *)"
-}
-
-variable "mf_elb_o365_ip_sync_schedule_enabled" {
-  type        = bool
-  description = "Enable or disable the mf elb o365 ip sync schedule"
-  default     = false
-}
-
 variable "tag_origin" {
   type = string
   # No default, set by tf-fsc.sh
