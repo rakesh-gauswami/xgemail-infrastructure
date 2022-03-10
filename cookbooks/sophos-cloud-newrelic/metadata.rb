800e5a0dbdee01206d0ed326bdb1557224c33877
mikedellacava <mike.dellacava@sophos.com>
2022-03-10 16:07:36 -0500

    MD-3444 new relic metadata

---
+++ cookbooks/sophos-cloud-newrelic/metadata.rb
@@ -0,0 +1,8 @@
+name 'sophos_cloud_newrelic'
+maintainer 'Sophos'
+license 'All Rights Reserved'
+description 'Installs/Configures New Relic'
+version '1.0.84'
+chef_version '>= 14.7'
+
+depends 'artifactory-artifact', '= 0.6.0'
