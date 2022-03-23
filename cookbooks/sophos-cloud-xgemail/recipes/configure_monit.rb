92b6f96b9c5e98783e21662207bf972f64e521d6
abhijit.sharma <abhijit.sharma@sophos.com>
2022-03-23 15:54:13 +0530

    MD-3425 update missing account name

--- cookbooks/sophos-cloud-xgemail/recipes/configure_monit.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure_monit.rb
@@ -13,7 +13,8 @@
 ::Chef::Recipe.send(:include, ::SophosCloudXgemail::Helper)
 ::Chef::Resource.send(:include, ::SophosCloudXgemail::Helper)
 
-NODE_TYPE = node['xgemail']['cluster_type']
+ACCOUNT_NAME = node['sophos_cloud']['account_name']
+NODE_TYPE    = node['xgemail']['cluster_type']
 
 INSTANCE_DATA = node['xgemail']['postfix_instance_data'][NODE_TYPE]
 raise "Unsupported node type [#{NODE_TYPE}]" if INSTANCE_DATA.nil?
