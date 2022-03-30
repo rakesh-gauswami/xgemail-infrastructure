334d12090476c8c8e07be3a6b8fb59bdd0003f18
Ryan Murphy <ryan.murphy@sophos.com>
2022-03-30 16:55:49 -0400

    MD-3539 Updated metadata file in newrelic cookbook

--- cookbooks/sophos-cloud-newrelic/metadata.rb
+++ cookbooks/sophos-cloud-newrelic/metadata.rb
@@ -1,7 +1,9 @@
-name 'sophos_cloud_newrelic'
-maintainer 'Sophos'
+name 'sophos-cloud-newrelic'
+maintainer 'Sophos Email'
 license 'All Rights Reserved'
 description 'Installs/Configures New Relic'
-version '1.0.84'
+version          '1.0.0'
 
-depends 'artifactory-artifact', '= 0.6.0'
+maintainer_email 'SophosMailOps@sophos.com'
+
+depends          'sophos-cloud-xgemail'
