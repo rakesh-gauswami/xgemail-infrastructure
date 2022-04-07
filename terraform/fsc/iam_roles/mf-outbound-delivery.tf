d98d2f08072b5b348a6a820b0e76c3288ffc0a96
Ryan Murphy <ryan.murphy@sophos.com>
2022-04-07 11:52:13 -0400

    MD-3539 Added IAM policy attachment for secretsmanager to mf-outbound-delivery Role

--- terraform/fsc/iam_roles/mf-outbound-delivery.tf
+++ terraform/fsc/iam_roles/mf-outbound-delivery.tf
@@ -63,3 +63,8 @@ resource "aws_iam_role_policy_attachment" "mf_outbound_delivery_instance_role_sn
   role       = aws_iam_role.mf_outbound_delivery_instance_role.id
   policy_arn = aws_iam_policy.sns_policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "mf_outbound_delivery_instance_role_secretsmanager_policy" {
+  role       = aws_iam_role.mf_outbound_delivery_instance_role.id
+  policy_arn = aws_iam_policy.secretsmanager_policy.arn
+}
