5adef1828c9942bfde2e2b535406c690d1eaf78e
Aaron Singh <aaron.singh@sophos.com>
2022-04-26 14:01:36 -0700

    updated Lambda with customer-delivery string, so it can assign EC2 an EIP

--- lambda/xgemail_eip_rotation.py
+++ lambda/xgemail_eip_rotation.py
@@ -186,7 +186,8 @@ def get_instances_by_name():
                     'CloudEmail:beta-delivery:*',
                     'CloudEmail:beta-xdelivery:*',
                     'CloudEmail:delta-delivery:*',
-                    'CloudEmail:delta-xdelivery:*'
+                    'CloudEmail:delta-xdelivery:*',
+                    'CloudEmail:customer-delivery:*'
                 ]
             }
         ]
