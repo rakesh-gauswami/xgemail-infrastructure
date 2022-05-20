28036609d455601745a2cbf99c59f767792d2c63
Lakhman Odedra <lakhman.odedra@sophos.com>
2022-05-20 16:54:18 +0530

    cron base transport file from s3 flat file

---
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_customer_delivery_transport_updater_cron.rb
@@ -0,0 +1,109 @@
+#
+# Cookbook Name:: sophos-cloud-xgemail
+# Recipe:: setup_customer_delivery_transport_updater_cron
+#
+# Copyright 2022, Sophos
+#
+# All rights reserved - Do Not Redistribute
+#
+# This recipe installs cron job to pull domain routing information from S3 flat file
+#
+
+# Include Helper library
+::Chef::Recipe.send(:include, ::SophosCloudXgemail::Helper)
+::Chef::Resource.send(:include, ::SophosCloudXgemail::Helper)
+
+NODE_TYPE = node['xgemail']['cluster_type']
+
+INSTANCE_DATA = node['xgemail']['postfix_instance_data'][NODE_TYPE]
+raise "Unsupported node type [#{NODE_TYPE}]" if INSTANCE_DATA.nil?
+
+INSTANCE_NAME = INSTANCE_DATA[:instance_name]
+raise "Invalid instance name for node type [#{NODE_TYPE}]" if INSTANCE_NAME.nil?
+
+ACCOUNT               = node['sophos_cloud']['environment']
+ACCOUNT_NAME          = node['sophos_cloud']['account_name']
+REGION                = node['sophos_cloud']['region']
+CONNECTIONS_BUCKET    = node['sophos_cloud']['connections']
+
+CRON_JOB_TIMEOUT      = node['xgemail']['cron_job_timeout']
+CRON_MINUTE_FREQUENCY = node['xgemail']['customer_delivery_transport_cron_minute_frequency']
+STATION_VPC_NAME      = node['xgemail']['station_vpc_name']
+XGEMAIL_FILES_DIR     = node['xgemail']['xgemail_files_dir']
+TRANSPORT_FILENAME    = 'transport'
+POLICY_BUCKET         = node['xgemail']['xgemail_policy_bucket_name']
+XGEMAIL_UTILS_DIR      = node['xgemail']['xgemail_utils_files_dir']
+CUSTOM_ROUTE_TRANSPORT_PATH  = node['xgemail']['custom_route_transport_path']
+
+CONFIGURATION_COMMANDS =
+  [
+    "transport_maps=hash:$config_directory/#{TRANSPORT_FILENAME}"
+  ]
+
+if ACCOUNT == 'sandbox'
+  TRANSPORT_FILE = "/etc/#{instance_name(INSTANCE_NAME)}/#{TRANSPORT_FILENAME}"
+  file TRANSPORT_FILE do
+    content "#{node['sandbox']['mail_transport_entry']}\n"
+    mode '0644'
+    owner 'root'
+  end
+  CONFIGURATION_COMMANDS.each do | cur |
+    execute print_postmulti_cmd( INSTANCE_NAME, "postconf '#{cur}'" )
+  end
+
+  execute 'build_postmap_for_transport' do
+    user 'root'
+    command <<-EOH
+          postmap #{TRANSPORT_FILE}
+    EOH
+  end
+
+  # Return, don't create CRON job
+  return
+
+end
+
+PACKAGE_DIR                    = "#{XGEMAIL_FILES_DIR}/customer-delivery-transport"
+TRANSPORT_UPDATER_SCRIPT       = 'customer.delivery.transport.flat.file.py'
+TRANSPORT_UPDATER_SCRIPT_PATH  = "#{PACKAGE_DIR}/#{TRANSPORT_UPDATER_SCRIPT}"
+
+directory XGEMAIL_FILES_DIR do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  recursive true
+end
+
+directory PACKAGE_DIR do
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
+template TRANSPORT_UPDATER_SCRIPT_PATH do
+  source "#{TRANSPORT_UPDATER_SCRIPT}.erb"
+  mode '0750'
+  owner 'root'
+  group 'root'
+  variables(
+    :postfix_instance_name => instance_name( INSTANCE_NAME ),
+    :transport_filename => TRANSPORT_FILENAME,
+    :connections_bucket => CONNECTIONS_BUCKET,
+    :policy_bucket => POLICY_BUCKET,
+    :xgemail_utils_path => XGEMAIL_UTILS_DIR,
+    :custom_route_transport_path => CUSTOM_ROUTE_TRANSPORT_PATH
+  )
+  notifies :run, "execute[#{TRANSPORT_UPDATER_SCRIPT_PATH}]", :immediately
+end
+
+CONFIGURATION_COMMANDS.each do | cur |
+  execute print_postmulti_cmd( INSTANCE_NAME, "postconf '#{cur}'" )
+end
+
+cron "#{INSTANCE_NAME}-transport-cron" do
+  minute "1-59/#{CRON_MINUTE_FREQUENCY}"
+  user 'root'
+  command "source /etc/profile && timeout #{CRON_JOB_TIMEOUT} flock --nb /var/lock/#{CRON_SCRIPT}.lock -c '#{CRON_SCRIPT_PATH}' >/dev/null 2>&1"
+  action :nothing
+end
+
