802cea04814a5e2f7497ec48746828ac1b738446
ABHIJIT SHARMA <abhijit.sharma@sophos.com>
2022-06-02 15:19:29 +0530

    MD-3625 update directory permission for swaks

--- cookbooks/sophos-cloud-xgemail/recipes/configure_swaks.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure_swaks.rb
@@ -39,21 +39,3 @@ bash 'download_swaks' do
     /bin/chmod +x /opt/swaks/swaks
   EOH
 end
-
-bash 'install_savi_client' do
-  user 'root'
-  cwd '/tmp'
-  code <<-EOH
-    set -e
-
-    mkdir -p /tmp/savi
-    aws --region us-west-2 s3 cp s3:#{node['sophos_cloud']['savi']} /tmp/savi
-    tar -xzvf /tmp/savi/savi-install.tar.gz -C /tmp/savi/
-
-    pushd /tmp/savi/savi-install
-    bash install.sh
-    popd
-
-    rm -rf /tmp/savi /tmp/savi-install.tar.gz
-  EOH
-end
