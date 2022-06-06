24ce0c8933f04f67924ff1623ca2678a28599d4a
Lakhman Odedra <lakhman.odedra@sophos.com>
2022-06-06 13:52:50 +0530

    flat file transport initial sync script added

---
+++ cookbooks/sophos-cloud-xgemail/recipes/setup_customer_delivery_initial_sync_transport.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: sophos-cloud-xgemail
+# Recipe:: setup_customer_delivery_initial_sync_transport
+#
+# Copyright 2022, Sophos
+#
+# All rights reserved - Do Not Redistribute
+#
+# This recipe configures the Script for initial sync of transport on customer delivery servers
+#
+
+require 'aws-sdk'
+require 'json'
+
+# Include Helper library
+::Chef::Recipe.send(:include, ::SophosCloudXgemail::Helper)
+::Chef::Resource.send(:include, ::SophosCloudXgemail::Helper)
+
+AWS_REGION              = node['sophos_cloud']['region']
+XGEMAIL_FILES_DIR       = node['xgemail']['xgemail_files_dir']
+XGEMAIL_UTILS_DIR       = node['xgemail']['xgemail_utils_files_dir']
+
+PACKAGE_DIR             = "#{XGEMAIL_FILES_DIR}/toggle-flat-file"
+INITIAL_SYNC_TRANSPORT_SCRIPT      = 'customer.delivery.initial.sync.transport.py'
+INITIAL_SYNC_TRANSPORT_SCRIPT_PATH = "#{PACKAGE_DIR}/#{INITIAL_SYNC_TRANSPORT_SCRIPT}"
+
+#directory for internet submit bulk release script
+directory PACKAGE_DIR do
+  mode '0755'
+  owner 'root'
+  group 'root'
+end
+
+execute INITIAL_SYNC_TRANSPORT_SCRIPT_PATH do
+  user 'root'
+  action :nothing
+end
+
+# Write bulk release script to internet submit instance
+template INITIAL_SYNC_TRANSPORT_SCRIPT_PATH do
+  source "#{BULK_RELEASE_SCRIPT}.erb"
+  mode '0750'
+  owner 'root'
+  group 'root'
+  variables(
+    :aws_region => AWS_REGION,
+    :xgemail_utils_path => XGEMAIL_UTILS_DIR
+  )
+end
+
