faae6e1cb3ed02f554b3107ea347431138e9d3f5
Ryan Murphy <ryan.murphy@sophos.com>
2022-07-08 15:30:00 -0400

    MD-3776 Updatedx metadata for sophos-cloud-fluentd cookbook

--- cookbooks/sophos-msg-fluentd/metadata.rb
+++ cookbooks/sophos-msg-fluentd/metadata.rb
@@ -1,10 +1,9 @@
-name             'sophos-msg-fluentd'
-maintainer       'Sophos'
-license          'All rights reserved'
-description      'Installs/Configures Fluentd/TD-Agent'
-version          '1.0.0'
+name              'sophos-msg-fluentd'
+maintainer        'Sophos Email'
+maintainer_email  'SophosMailOps@sophos.com'
+license           'All rights reserved'
+description       'Installs/Configures Fluentd/TD-Agent'
+long_description  'Installs/Configures Fluentd/TD-Agent'
+version           '1.0.0'
 
-maintainer 'Sophos Email'
-maintainer_email 'sophos-email-dev@sophos.com'
-
-depends          'sophos-cloud-xgemail'
+depends           'sophos-cloud-xgemail'
