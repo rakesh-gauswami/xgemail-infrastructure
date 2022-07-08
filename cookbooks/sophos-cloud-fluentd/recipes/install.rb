ada9a4bb2bc54f5099fdff98ec65e16ecdc98f89
Swaroop Mallenahalli <Swaroop.M@CA-MN-43000.local>
2022-07-08 12:38:51 -0700

    update

--- cookbooks/sophos-cloud-fluentd/recipes/install.rb
+++ cookbooks/sophos-cloud-fluentd/recipes/install.rb
@@ -219,8 +219,10 @@ ruby_block 'edit rsyslog.conf' do
       '#$ModLoad imjournal # provides access to the systemd journal')
     file.search_file_replace_line(
       '^\$ActionFileDefaultTemplate RSYSLOG_TraditionalFileFormat',
-      '#$template CustomFileFormat,"%timegenerated:1:3:date-rfc3164% %$day% %timegenerated:12:23:date-rfc3339% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n"
-       $ActionFileDefaultTemplate CustomFileFormat')
+      '#$template CustomFileFormat,"%timegenerated:1:3:date-rfc3164% %$day% %timegenerated:12:23:date-rfc3339% %HOSTNAME% %syslogtag%%msg:::sp-if-no-1st-sp%%msg:::drop-last-lf%\n"')
+    file.insert_line_if_no_match(
+      '^\$ActionFileDefaultTemplate CustomFileFormat',
+      '$ActionFileDefaultTemplate CustomFileFormat')
     file.write_file
   end
 end
