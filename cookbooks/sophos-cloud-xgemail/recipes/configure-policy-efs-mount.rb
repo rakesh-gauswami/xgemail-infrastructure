ebc79c3968c66770f0c029eca43800d4b5adf75c
Mike.Dellacava <Mike.Dellacava@sophos.com>
2022-08-02 17:11:16 -0400

    MD-4196 Add new postfix queue EFS volume to internet submit initially

--- cookbooks/sophos-cloud-xgemail/recipes/configure-policy-efs-mount.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-policy-efs-mount.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: sophos-cloud-xgemail
 # Recipe:: configure-policy-efs-mount.rb
 #
-# Copyright 2021, Sophos
+# Copyright 2022, Sophos
 #
 # All rights reserved - Do Not Redistribute
 #
