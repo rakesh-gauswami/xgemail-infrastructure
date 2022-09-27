327814bbad6ff2149aa1cc1616d1afad1d0d50c4
girijasankar.panda <girijasankar.panda@sophos.com>
2022-09-27 11:34:51 +0530

    XGE-25990: Added server side TLS config for postfix in outbound delivery servers

--- cookbooks/sophos-cloud-xgemail/recipes/configure-beta-delivery-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-beta-delivery-queue.rb
@@ -83,6 +83,7 @@ CONFIGURATION_COMMANDS =
     'smtp_tls_ciphers=high',
     'smtp_tls_mandatory_ciphers=high',
     'smtp_tls_mandatory_protocols = TLSv1.2',
+    'smtp_tls_CAfile = /etc/pki/tls/certs/ca-bundle.crt',
     'smtp_tls_loglevel=1',
     'smtp_tls_session_cache_database=btree:${data_directory}/smtp-tls-session-cache',
     "header_checks = regexp:#{HEADER_CHECKS_PATH}"
