7dc7e6ae01041143d7e2495336de268708fa3d55
girijasankar.panda <girijasankar.panda@sophos.com>
2022-10-04 12:45:52 +0530

    XGE-25990: Reverting changes for mf outbound x delivery

--- cookbooks/sophos-cloud-xgemail/recipes/configure-extended-delivery-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-extended-delivery-queue.rb
@@ -156,7 +156,7 @@ end
   execute print_postmulti_cmd( INSTANCE_NAME, "postconf -P '#{cur}'" )
 end
 
-if NODE_TYPE == 'mf-inbound-xdelivery'
+if NODE_TYPE == 'mf-inbound-xdelivery' || NODE_TYPE == 'mf-outbound-xdelivery'
   [
       # Server side TLS configuration
       'smtpd_tls_security_level = may',
@@ -180,31 +180,6 @@ if NODE_TYPE == 'mf-inbound-xdelivery'
   ].each do |cur|
     execute print_postmulti_cmd(INSTANCE_NAME, "postconf '#{cur}'")
   end
-elsif  NODE_TYPE == 'mf-outbound-xdelivery'
- [
-     # Server side TLS configuration
-     'smtpd_tls_security_level = may',
-     'smtpd_tls_ciphers = high',
-     'smtpd_tls_mandatory_ciphers = high',
-     'smtpd_tls_loglevel = 1',
-     'smtpd_tls_received_header = yes',
-     "smtpd_tls_cert_file = #{SERVER_PEM_FILE}",
-     "smtpd_tls_key_file = #{KEY_FILE}",
-     'bounce_queue_lifetime=0',
-     "hopcount_limit = #{HOP_COUNT_DELIVERY_INSTANCE}",
-     'mynetworks = 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16',
-     'smtp_tls_security_level = encrypt',
-     'smtp_tls_ciphers=high',
-     'smtp_tls_mandatory_ciphers = high',
-     'smtp_tls_mandatory_protocols = !SSLv2,!SSLv3,!TLSv1,!TLSv1.1,TLSv1.2',
-     'smtp_tls_CAfile = /etc/pki/tls/certs/ca-bundle.crt',
-     'smtp_tls_protocols = !SSLv2,!SSLv3,!TLSv1,!TLSv1.1,TLSv1.2',
-     'smtp_tls_loglevel=1',
-     'smtp_tls_session_cache_database=btree:${data_directory}/smtp-tls-session-cache',
-     'soft_bounce=yes'
- ].each do |cur|
-   execute print_postmulti_cmd(INSTANCE_NAME, "postconf '#{cur}'")
- end
 else
   [
       # Server side TLS configuration
@@ -231,7 +206,7 @@ else
 end
 
 
-if NODE_TYPE == 'internet-xdelivery' || NODE_TYPE == 'risky-xdelivery' || NODE_TYPE == 'warmup-xdelivery' || NODE_TYPE == 'beta-xdelivery' || NODE_TYPE == 'delta-xdelivery' || NODE_TYPE == 'mf-outbound-xdelivery'
+if NODE_TYPE == 'internet-xdelivery' || NODE_TYPE == 'risky-xdelivery' || NODE_TYPE == 'warmup-xdelivery' || NODE_TYPE == 'beta-xdelivery' || NODE_TYPE == 'delta-xdelivery'
 
   HEADER_CHECKS_PATH = "/etc/postfix-#{INSTANCE_NAME}/header_checks"
 
@@ -243,6 +218,24 @@ if NODE_TYPE == 'internet-xdelivery' || NODE_TYPE == 'risky-xdelivery' || NODE_T
 /^X_Sophos_TLS_Connection: PRE_TLS_1.3$/i FILTER pref_tls_13:
 /^X_Sophos_TLS_Connection: PRE_TLS_1.3_V$/i FILTER pref_tls_13_verify:
 /^X-Sophos-Enforce-TLS: yes$|^X-Sophos-TLS-Probe: SUCCESS$|^X_Sophos_TLS_Connection: TLS_1_2$/i FILTER smtp_encrypt:"
+    mode '0644'
+    owner 'root'
+    group 'root'
+  end
+
+  [
+    "header_checks = regexp:#{HEADER_CHECKS_PATH}"
+  ].each do | cur |
+    execute print_postmulti_cmd( INSTANCE_NAME, "postconf '#{cur}'" )
+  end
+end
+
+if NODE_TYPE == 'mf-outbound-xdelivery'
+
+  HEADER_CHECKS_PATH = "/etc/postfix-#{INSTANCE_NAME}/header_checks"
+
+  file "#{HEADER_CHECKS_PATH}" do
+    content "/^X-Sophos-Enforce-TLS: yes$|^X-Sophos-TLS-Probe: SUCCESS$/i FILTER smtp_encrypt:"
     mode '0644'
     owner 'root'
     group 'root'
