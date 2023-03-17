b78df07c7d49f5140a863f650cb51863066f991d
deepak.kharpuriya <deepak.kharpuriya@sophos.com>
2023-03-17 17:34:56 +0530

    removed from risky and warmup delivery

--- cookbooks/sophos-cloud-xgemail/recipes/configure-warmup-delivery-queue.rb
+++ cookbooks/sophos-cloud-xgemail/recipes/configure-warmup-delivery-queue.rb
@@ -55,8 +55,7 @@ file "#{HEADER_CHECKS_PATH}" do
 /^X_Sophos_TLS_Connection: TLS_1_3_V$/i FILTER tls_13_verify:
 /^X_Sophos_TLS_Connection: PRE_TLS_1.3$/i FILTER pref_tls_13:
 /^X_Sophos_TLS_Connection: PRE_TLS_1.3_V$/i FILTER pref_tls_13_verify:
-/^X-Sophos-Enforce-TLS: yes$|^X-Sophos-TLS-Probe: SUCCESS$|^X_Sophos_TLS_Connection: TLS_1_2$/i FILTER smtp_encrypt:
-/^X-Sophos-Email-Transport-Route: (.*)$/i FILTER $1"
+/^X-Sophos-Enforce-TLS: yes$|^X-Sophos-TLS-Probe: SUCCESS$|^X_Sophos_TLS_Connection: TLS_1_2$/i FILTER smtp_encrypt:"
   mode '0644'
   owner 'root'
   group 'root'
